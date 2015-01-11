a56bf342f8c76db5ae9076b642e9c4c578eedc42
Dan Markwat <daniel.markwat@gmail.com>
2015-01-11 18:51:45 -0500

    Updated the GraphService configurator and removed the need for an extra properties file

--- repository/src/main/java/org/apache/hadoop/metadata/services/GraphServiceConfigurator.java
+++ repository/src/main/java/org/apache/hadoop/metadata/services/GraphServiceConfigurator.java
@@ -1,9 +1,11 @@
 package org.apache.hadoop.metadata.services;
 
+import com.thinkaurelius.titan.core.TitanGraph;
+
 public class GraphServiceConfigurator extends PropertyBasedConfigurator<GraphService> {
 	private static final String PROPERTY_NAME = "metadata.graph.impl.class";
-	private static final String DEFAULT_IMPL_CLASS = "no.default.graph.class";
-	private static final String CONFIG_PATH = "metadata.graph.properties";
+	private static final String DEFAULT_IMPL_CLASS = TitanGraph.class.getName();
+	private static final String CONFIG_PATH = "application.properties";
 
 	public GraphServiceConfigurator() {
 		super("metadata.graph.propertyName", "metadata.graph.defaultImplClass",
