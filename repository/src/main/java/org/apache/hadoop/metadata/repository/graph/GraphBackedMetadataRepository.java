3ba61b9e7d7427068f766a4e6d5ee5bf82206778
Venkatesh Seetharam <venkatesh@apache.org>
2015-01-27 17:22:10 -0800

    Fixes a bug in empty collections. Contributed by Venkatesh Seetharam

--- repository/src/main/java/org/apache/hadoop/metadata/repository/graph/GraphBackedMetadataRepository.java
+++ repository/src/main/java/org/apache/hadoop/metadata/repository/graph/GraphBackedMetadataRepository.java
@@ -463,7 +463,7 @@ public class GraphBackedMetadataRepository implements MetadataRepository {
             LOG.debug("Mapping instance {} to vertex {} for name {}",
                     typedInstance.getTypeName(), instanceVertex, attributeInfo.name);
             List list = (List) typedInstance.get(attributeInfo.name);
-            if (list == null) {
+            if (list == null || list.isEmpty()) {
                 return;
             }
 
@@ -495,7 +495,7 @@ public class GraphBackedMetadataRepository implements MetadataRepository {
                     typedInstance.getTypeName(), instanceVertex, attributeInfo.name);
             @SuppressWarnings("unchecked")
             Map<Object, Object> collection = (Map<Object, Object>) typedInstance.get(attributeInfo.name);
-            if (collection == null) {
+            if (collection == null || collection.isEmpty()) {
                 return;
             }
 
