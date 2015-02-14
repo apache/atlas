3e9bb4c29847168447c7108f4f536af3cfb9cf73
Vishal Kadam <vishal.4kadam@gmail.com>
2015-02-14 14:34:29 -0500

    Added check for dist directory and added common.css

--- dgi-metadata-ui/gruntfile.js
+++ dgi-metadata-ui/gruntfile.js
@@ -2,7 +2,7 @@
 
 var git = require('git-rev');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     var classPathSep = (process.platform === "win32") ? ';' : ':',
         gitHash = '',
         pkg = grunt.file.readJSON('package.json');
@@ -92,16 +92,16 @@ module.exports = function(grunt) {
         shell: {
             min: {
                 command: 'java ' +
-                    '-cp public/lib/closure-compiler/compiler.jar' + classPathSep +
-                    'public/lib/ng-closure-runner/ngcompiler.jar ' +
-                    'org.angularjs.closurerunner.NgClosureRunner ' +
-                    '--compilation_level SIMPLE_OPTIMIZATIONS ' +
+                '-cp public/lib/closure-compiler/compiler.jar' + classPathSep +
+                'public/lib/ng-closure-runner/ngcompiler.jar ' +
+                'org.angularjs.closurerunner.NgClosureRunner ' +
+                '--compilation_level SIMPLE_OPTIMIZATIONS ' +
                     //'--formatting PRETTY_PRINT ' +
-                    '--language_in ECMASCRIPT5_STRICT ' +
-                    '--angular_pass ' +
-                    '--manage_closure_dependencies ' +
-                    '--js <%= modules %> ' +
-                    '--js_output_file <%= dist %>'
+                '--language_in ECMASCRIPT5_STRICT ' +
+                '--angular_pass ' +
+                '--manage_closure_dependencies ' +
+                '--js <%= modules %> ' +
+                '--js_output_file <%= dist %>'
             }
         },
         devUpdate: {
@@ -114,7 +114,7 @@ module.exports = function(grunt) {
         compress: {
             release: {
                 options: {
-                    archive: function() {
+                    archive: function () {
                         return [pkg.name, pkg.version, gitHash].join('_') + '.tgz';
                     }
                 },
@@ -127,15 +127,22 @@ module.exports = function(grunt) {
 
     grunt.registerTask('default', ['devUpdate', 'bower', 'jshint', 'jsbeautifier:default']);
 
-    grunt.registerTask('server', ['bower', 'jshint', 'shell:min', 'concurrent']);
+    grunt.registerTask('server', ['bower', 'jshint', 'minify', 'concurrent']);
     grunt.registerTask('server:prod', ['nodemon:prod']);
     grunt.registerTask('server:prod', ['nodemon:prod']);
 
-    grunt.registerTask('release', 'Create release package', function() {
+    grunt.registerTask('minify', 'Minify the all js', function () {
         var done = this.async();
-        git.short(function(str) {
+        grunt.file.mkdir('public/dist');
+        grunt.task.run(['shell:min']);
+        done();
+    });
+
+    grunt.registerTask('release', 'Create release package', function () {
+        var done = this.async();
+        git.short(function (str) {
             gitHash = str;
-            grunt.task.run(['shell:min', 'compress:release']);
+            grunt.task.run(['minify', 'compress:release']);
             done();
         });
     });
