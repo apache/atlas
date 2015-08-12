48343db999b495458409644c8b9d2fd0bd9fa99d
Shwetha GS <sshivalingamurthy@hortonworks.com>
2015-08-12 11:15:02 +0530

    ATLAS-91 Add solr configuration and documentation (suma.shivaprasad via shwethags)

--- repository/src/main/java/org/apache/atlas/repository/graph/TitanGraphProvider.java
+++ repository/src/main/java/org/apache/atlas/repository/graph/TitanGraphProvider.java
@@ -18,9 +18,12 @@
 
 package org.apache.atlas.repository.graph;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.Provides;
 import com.thinkaurelius.titan.core.TitanFactory;
 import com.thinkaurelius.titan.core.TitanGraph;
+import com.thinkaurelius.titan.diskstorage.StandardIndexProvider;
+import com.thinkaurelius.titan.diskstorage.solr.Solr5Index;
 import org.apache.atlas.ApplicationProperties;
 import org.apache.atlas.AtlasException;
 import org.apache.commons.configuration.Configuration;
@@ -28,6 +31,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Singleton;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Default implementation for Graph Provider that doles out Titan Graph.
@@ -46,6 +53,35 @@ public class TitanGraphProvider implements GraphProvider<TitanGraph> {
     private static Configuration getConfiguration() throws AtlasException {
         Configuration configProperties = ApplicationProperties.get();
         return ApplicationProperties.getSubsetConfiguration(configProperties, GRAPH_PREFIX);
+    }
+
+    static {
+        addSolr5Index();
+    }
+
+    /**
+     * Titan loads index backend name to implementation using StandardIndexProvider.ALL_MANAGER_CLASSES
+     * But StandardIndexProvider.ALL_MANAGER_CLASSES is a private static final ImmutableMap
+     * Only way to inject Solr5Index is to modify this field. So, using hacky reflection to add Sol5Index
+     */
+    private static void addSolr5Index() {
+        try {
+            Field field = StandardIndexProvider.class.getDeclaredField("ALL_MANAGER_CLASSES");
+            field.setAccessible(true);
+
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+            Map<String, String> customMap = new HashMap(StandardIndexProvider.getAllProviderClasses());
+            customMap.put("solr5", Solr5Index.class.getName());
+            ImmutableMap<String, String> immap = ImmutableMap.copyOf(customMap);
+            field.set(null, immap);
+
+            LOG.debug("Injected solr5 index - {}", Solr5Index.class.getName());
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
