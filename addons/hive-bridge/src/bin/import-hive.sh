27a39063d17ceaa32b602f2661d7a7ffb6dbf3ab
Suma Shivaprasad <sumasai.shivaprasad@gmail.com>
2015-11-06 14:31:57 +0530

    ATLAS-257 import_hive.sh fails when run under cygwin(dkantor via sumasai)

--- addons/hive-bridge/src/bin/import-hive.sh
+++ addons/hive-bridge/src/bin/import-hive.sh
@@ -16,6 +16,8 @@
 # resolve links - $0 may be a softlink
 PRG="${0}"
 
+[[ `uname -s` == *"CYGWIN"* ]] && CYGWIN=true
+
 while [ -h "${PRG}" ]; do
   ls=`ls -ld "${PRG}"`
   link=`expr "$ls" : '.*-> \(.*\)$'`
@@ -38,21 +40,23 @@ if [ -f "${METADATA_CONF}/atlas-env.sh" ]; then
   . "${METADATA_CONF}/atlas-env.sh"
 fi
 
-if test -z ${JAVA_HOME}
+if test -z "${JAVA_HOME}"
 then
     JAVA_BIN=`which java`
     JAR_BIN=`which jar`
 else
-    JAVA_BIN=${JAVA_HOME}/bin/java
-    JAR_BIN=${JAVA_HOME}/bin/jar
+    JAVA_BIN="${JAVA_HOME}/bin/java"
+    JAR_BIN="${JAVA_HOME}/bin/jar"
 fi
 export JAVA_BIN
 
-if [ ! -e $JAVA_BIN ] || [ ! -e $JAR_BIN ]; then
+if [ ! -e "${JAVA_BIN}" ] || [ ! -e "${JAR_BIN}" ]; then
   echo "$JAVA_BIN and/or $JAR_BIN not found on the system. Please make sure java and jar commands are available."
   exit 1
 fi
 
+# Construct classpath using Atlas conf directory
+# and jars from bridge/hive and hook/hive directories.
 METADATACPPATH="$METADATA_CONF"
 
 for i in "${BASEDIR}/bridge/hive/"*.jar; do
@@ -66,14 +70,8 @@ done
 # log dir for applications
 METADATA_LOG_DIR="${METADATA_LOG_DIR:-$BASEDIR/logs}"
 export METADATA_LOG_DIR
+LOGFILE="$METADATA_LOG_DIR/import-hive.log"
 
-JAVA_PROPERTIES="$METADATA_OPTS -Datlas.log.dir=$METADATA_LOG_DIR -Datlas.log.file=import-hive.log -Dlog4j.configuration=atlas-log4j.xml"
-shift
-
-while [[ ${1} =~ ^\-D ]]; do
-  JAVA_PROPERTIES="${JAVA_PROPERTIES} ${1}"
-  shift
-done
 TIME=`date +%Y%m%d%H%M%s`
 
 #Add hive conf in classpath
@@ -88,10 +86,30 @@ else
     exit 1
 fi
 export HIVE_CP
-echo Using Hive configuration directory [$HIVE_CP]
-echo "Logs for import are in $METADATA_LOG_DIR/import-hive.log"
 
-${JAVA_BIN} ${JAVA_PROPERTIES} -cp ${HIVE_CP}:${METADATACPPATH} org.apache.atlas.hive.bridge.HiveMetaStoreBridge
+CP="${HIVE_CP}:${METADATACPPATH}"
+
+# If running in cygwin, convert pathnames and classpath to Windows format.
+if [ "${CYGWIN}" == "true" ]
+then
+   METADATA_LOG_DIR=`cygpath -w ${METADATA_LOG_DIR}`
+   LOGFILE=`cygpath -w ${LOGFILE}`
+   HIVE_CP=`cygpath -w ${HIVE_CP}`
+   CP=`cygpath -w -p ${CP}`
+fi
+
+JAVA_PROPERTIES="$METADATA_OPTS -Datlas.log.dir=$METADATA_LOG_DIR -Datlas.log.file=import-hive.log -Dlog4j.configuration=atlas-log4j.xml"
+shift
+
+while [[ ${1} =~ ^\-D ]]; do
+  JAVA_PROPERTIES="${JAVA_PROPERTIES} ${1}"
+  shift
+done
+
+echo Using Hive configuration directory ["$HIVE_CP"]
+echo "Log file for import is $LOGFILE"
+
+"${JAVA_BIN}" ${JAVA_PROPERTIES} -cp "${CP}" org.apache.atlas.hive.bridge.HiveMetaStoreBridge
 
 RETVAL=$?
 [ $RETVAL -eq 0 ] && echo Hive Data Model imported successfully!!!
