9dc4cfbcb10f38cbf8fa331fe76093d9791d6204
Suma Shivaprasad <sumasai.shivaprasad@gmail.com>
2016-10-31 14:42:13 -0700

    ATLAS-1239 when stopping Atlas on the command line it should explicitly say when it has stopped (ayubkhan via sumasai)

--- distro/src/bin/atlas_stop.py
+++ distro/src/bin/atlas_stop.py
@@ -49,6 +49,8 @@ def main():
     os.kill(pid, SIGTERM)
 
     mc.wait_for_shutdown(pid, "stopping atlas", 30)
+    if not mc.exist_pid(pid):
+        print "Apache Atlas Server stopped!!!\n"
 
     # assuming kill worked since process check on windows is more involved...
     if os.path.exists(atlas_pid_file):
