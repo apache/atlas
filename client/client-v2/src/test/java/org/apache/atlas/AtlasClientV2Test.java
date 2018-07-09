efce210e094f78f935af26cf3d71fa66af281cf9
Kwangsiklee <lks21c@gmail.com>
2018-07-09 16:32:38 +0900

    Add initial test case for AtlasClientV2
    This is a initial test case for AtlasClientV2.

---
+++ client/client-v2/src/test/java/org/apache/atlas/AtlasClientV2Test.java
@@ -0,0 +1,18 @@
+package org.apache.atlas;
+
+import org.apache.commons.configuration.Configuration;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AtlasClientV2Test {
+
+    public static final String ATLAS_ENDPOINT = "atlas.rest.address";
+
+    @Test
+    public void testName() throws AtlasServiceException, AtlasException {
+        Configuration configuration = ApplicationProperties.get();
+        String[] atlasEndPoint = configuration.getStringArray(ATLAS_ENDPOINT);
+        AtlasClientV2 atlasClientV2 = new AtlasClientV2(atlasEndPoint, new String[]{"admin", "admin"});
+        Assert.assertTrue(atlasClientV2.isServerReady());
+    }
+}
