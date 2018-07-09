04b16c3e25b75194b342a1076de485b60dc3819c
Kwangsiklee <lks21c@gmail.com>
2018-07-09 16:48:24 +0900

    change test codes

--- client/client-v2/src/test/java/org/apache/atlas/AtlasClientV2Test.java
+++ client/client-v2/src/test/java/org/apache/atlas/AtlasClientV2Test.java
@@ -2,17 +2,24 @@ package org.apache.atlas;
 
 import org.apache.commons.configuration.Configuration;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class AtlasClientV2Test {
 
     public static final String ATLAS_ENDPOINT = "atlas.rest.address";
 
-    @Test
-    public void testIsServerReady() throws AtlasServiceException, AtlasException {
+    AtlasClientV2 atlasClientV2;
+
+    @BeforeClass
+    public void testName() throws AtlasException {
         Configuration configuration = ApplicationProperties.get();
         String[] atlasEndPoint = configuration.getStringArray(ATLAS_ENDPOINT);
-        AtlasClientV2 atlasClientV2 = new AtlasClientV2(atlasEndPoint, new String[]{"admin", "admin"});
+        atlasClientV2 = new AtlasClientV2(atlasEndPoint, new String[]{"admin", "admin"});
+    }
+
+    @Test
+    public void testIsServerReady() throws AtlasServiceException {
         Assert.assertTrue(atlasClientV2.isServerReady());
     }
 }
