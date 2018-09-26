09690bb402fb41677504dec24b47d6a60bb3fcfe
Ashutosh Mestry <amestry@hortonworks.com>
2018-09-26 14:24:13 -0700

    ATLAS-2895: Server full name processing

--- intg/src/main/java/org/apache/atlas/model/impexp/AtlasExportRequest.java
+++ intg/src/main/java/org/apache/atlas/model/impexp/AtlasExportRequest.java
@@ -82,7 +82,7 @@ public class AtlasExportRequest implements Serializable {
     }
 
     public String getFetchTypeOptionValue() {
-        if(getOptions() == null || !getOptions().containsKey(OPTION_FETCH_TYPE)) {
+        if(MapUtils.isEmpty(getOptions()) || !getOptions().containsKey(OPTION_FETCH_TYPE)) {
             return FETCH_TYPE_FULL;
         }
 
@@ -95,7 +95,8 @@ public class AtlasExportRequest implements Serializable {
     }
 
     public boolean getSkipLineageOptionValue() {
-        if(!getOptions().containsKey(AtlasExportRequest.OPTION_SKIP_LINEAGE)) {
+        if(MapUtils.isEmpty(getOptions()) ||
+                !getOptions().containsKey(AtlasExportRequest.OPTION_SKIP_LINEAGE)) {
             return false;
         }
 
@@ -124,12 +125,13 @@ public class AtlasExportRequest implements Serializable {
     }
 
     public long getChangeTokenFromOptions() {
-        if(getFetchTypeOptionValue().equalsIgnoreCase(FETCH_TYPE_INCREMENTAL) &&
-                getOptions().containsKey(AtlasExportRequest.FETCH_TYPE_INCREMENTAL_CHANGE_MARKER)) {
-            return Long.parseLong(getOptions().get(AtlasExportRequest.FETCH_TYPE_INCREMENTAL_CHANGE_MARKER).toString());
+        if (MapUtils.isEmpty(getOptions()) ||
+                !getFetchTypeOptionValue().equalsIgnoreCase(FETCH_TYPE_INCREMENTAL) ||
+                !getOptions().containsKey(AtlasExportRequest.FETCH_TYPE_INCREMENTAL_CHANGE_MARKER)) {
+            return 0L;
         }
 
-        return 0L;
+        return Long.parseLong(getOptions().get(AtlasExportRequest.FETCH_TYPE_INCREMENTAL_CHANGE_MARKER).toString());
     }
 
     public StringBuilder toString(StringBuilder sb) {
