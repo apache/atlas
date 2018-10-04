7ffbec1a2a16f5288fff27d5bb81254d3280a900
Ashutosh Mestry <amestry@hortonworks.com>
2018-10-04 15:30:13 -0700

    ATLAS-2897: Better handling of empty zip files. Unit test fix.

--- repository/src/test/java/org/apache/atlas/repository/impexp/ExportServiceTest.java
+++ repository/src/test/java/org/apache/atlas/repository/impexp/ExportServiceTest.java
@@ -218,7 +218,7 @@ public class ExportServiceTest extends ExportImportTestBase {
         assertNotNull(result.getSourceClusterName());
     }
 
-    @Test
+    @Test(expectedExceptions = AtlasBaseException.class)
     public void requestingEntityNotFound_NoData() throws AtlasBaseException, IOException {
         String requestingIP = "1.0.0.0";
         String hostName = "root";
@@ -231,11 +231,7 @@ public class ExportServiceTest extends ExportImportTestBase {
         Assert.assertNull(result.getData());
 
         ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-        ZipSource zipSource = new ZipSource(bais);
-
-        assertNotNull(exportService);
-        assertNotNull(zipSource.getCreationOrder());
-        Assert.assertFalse(zipSource.hasNext());
+        new ZipSource(bais);
     }
 
     @Test
@@ -306,14 +302,11 @@ public class ExportServiceTest extends ExportImportTestBase {
                 AtlasExportResult.OperationStatus.FAIL));
     }
 
-    @Test
+    @Test(expectedExceptions = AtlasBaseException.class)
     public void requestingExportOfNonExistentEntity_ReturnsFailure() throws Exception {
         AtlasExportRequest request = getRequestForEmployee();
         tamperEmployeeRequest(request);
-        ZipSource zipSource = runExportWithParameters(request);
-
-        assertNotNull(zipSource.getCreationOrder());
-        assertEquals(zipSource.getCreationOrder().size(), 0);
+        runExportWithParameters(request);
     }
 
     @Test
