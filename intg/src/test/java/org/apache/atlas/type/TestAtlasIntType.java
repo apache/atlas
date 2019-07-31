070d5972d4fba7a85370424c71821f3d17dc3c81
Mandar Ambawane <mandar.ambawane@freestoneinfotech.com>
2019-07-31 18:37:01 +0530

    ATLAS-3347: fixed range-validation for primitive attribute types
    (cherry picked from commit 6598c749f18a7bcd40d1c0a434d8c06a68deaa72)

--- intg/src/test/java/org/apache/atlas/type/TestAtlasIntType.java
+++ intg/src/test/java/org/apache/atlas/type/TestAtlasIntType.java
@@ -36,12 +36,19 @@ public class TestAtlasIntType {
         Double.valueOf(1), BigInteger.valueOf(1), BigDecimal.valueOf(1), "1",
     };
 
+    private final Object[] validValuesLimitCheck = {Byte.MIN_VALUE, Byte.MAX_VALUE, Short.MIN_VALUE, Short.MAX_VALUE,
+            Integer.MIN_VALUE, Integer.MAX_VALUE, Float.MIN_VALUE, Double.MIN_VALUE};
+
     private final Object[] negativeValues = {
         Byte.valueOf((byte)-1), Short.valueOf((short)-1), Integer.valueOf(-1), Long.valueOf(-1L), Float.valueOf(-1),
         Double.valueOf(-1), BigInteger.valueOf(-1), BigDecimal.valueOf(-1), "-1",
     };
 
-    private final Object[] invalidValues = { "", "12ab", "abcd", "-12ab", };
+    private  final Object[] negativeValuesLimitCheck = {-Float.MIN_VALUE, -Double.MIN_VALUE};
+
+    private final Object[] invalidValues = {"", "12ab", "abcd", "-12ab",
+            (long)Integer.MIN_VALUE - 1, (long)Integer.MAX_VALUE + 1,
+            Long.MIN_VALUE, Long.MAX_VALUE, Float.MAX_VALUE, Double.MAX_VALUE, -Float.MAX_VALUE, -Double.MAX_VALUE};
 
 
     @Test
@@ -57,7 +64,15 @@ public class TestAtlasIntType {
             assertTrue(intType.isValidValue(value), "value=" + value);
         }
 
+        for (Object value : validValuesLimitCheck) {
+            assertTrue(intType.isValidValue(value), "value=" + value);
+        }
+
         for (Object value : negativeValues) {
+            assertTrue(intType.isValidValue(value), "value=" + value);
+        }
+
+        for (Object value : negativeValuesLimitCheck) {
             assertTrue(intType.isValidValue(value), "value=" + value);
         }
 
@@ -81,11 +96,44 @@ public class TestAtlasIntType {
             assertEquals(normalizedValue, Integer.valueOf(1), "value=" + value);
         }
 
+        for (Object value : validValuesLimitCheck) {
+            if (value == null) {
+                continue;
+            }
+
+            Integer normalizedValue = intType.getNormalizedValue(value);
+
+            assertNotNull(normalizedValue, "value=" + value);
+
+            int i;
+            if (value instanceof Float) {
+                i = ((Float) value).intValue();
+                assertEquals(normalizedValue, Integer.valueOf(i), "value=" + value);
+            } else if (value instanceof Double) {
+                i = ((Double) value).intValue();
+                assertEquals(normalizedValue, Integer.valueOf(i), "value=" + value);
+            } else {
+                assertEquals(normalizedValue, Integer.valueOf(value.toString()), "value=" + value);
+            }
+        }
+
         for (Object value : negativeValues) {
             Integer normalizedValue = intType.getNormalizedValue(value);
 
             assertNotNull(normalizedValue, "value=" + value);
             assertEquals(normalizedValue, Integer.valueOf(-1), "value=" + value);
+        }
+
+        for (Object value : negativeValuesLimitCheck) {
+            Integer normalizedValue = intType.getNormalizedValue(value);
+            int i;
+            if (value instanceof Float) {
+                i = ((Float) value).intValue();
+                assertEquals(normalizedValue, Integer.valueOf(i), "value=" + value);
+            } else if (value instanceof Double) {
+                i = ((Double) value).intValue();
+                assertEquals(normalizedValue, Integer.valueOf(i), "value=" + value);
+            }
         }
 
         for (Object value : invalidValues) {
@@ -101,7 +149,17 @@ public class TestAtlasIntType {
             assertEquals(messages.size(), 0, "value=" + value);
         }
 
+        for (Object value : validValuesLimitCheck) {
+            assertTrue(intType.validateValue(value, "testObj", messages));
+            assertEquals(messages.size(), 0, "value=" + value);
+        }
+
         for (Object value : negativeValues) {
+            assertTrue(intType.validateValue(value, "testObj", messages));
+            assertEquals(messages.size(), 0, "value=" + value);
+        }
+
+        for (Object value : negativeValuesLimitCheck) {
             assertTrue(intType.validateValue(value, "testObj", messages));
             assertEquals(messages.size(), 0, "value=" + value);
         }
