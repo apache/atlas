e01d6b0d394ea955ce1be010ea87faebb2fc3a58
sameer79 <findsameershaikh@yahoo.co.in>
2019-11-28 12:01:26 +0530

    ATLAS-3531 : UI : Atlas website: download/release page missing releases.

--- docs/theme/components/shared/Header/HeaderDropDown/Basic.js
+++ docs/theme/components/shared/Header/HeaderDropDown/Basic.js
@@ -16,52 +16,126 @@
  * limitations under the License.
  */
 
-import Select from "react-dropdown-select";
-import React, { Fragment, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
-import * as colors from "../../../../styles/colors";
 
 const Basic = props => {
-  let { options, ...rest } = props;
-  const [value, setValue] = useState([
-    { id: 1, href: "/", title: "Latest", label: "Latest" }
-  ]);
+  let { options} = props;
+  const [toggle, setToggle] = useState(false);
+  const dropdownRef = useRef();
+  useEffect(() => {
+    const setToggleOnEvent = event => {
+      if (!(event.target.classList.contains('dropbtn') || event.target.classList.contains('dropdown-item')) && dropdownRef.current.classList.contains('show')) {
+        setToggle(false);
+      }
+    };
+    window.addEventListener("mousedown", setToggleOnEvent);
+    return () => {
+      window.removeEventListener("mousedown", setToggleOnEvent);
+    };
+  }, []);
 
   const ComponentStyle = styled.div`
     > div {
       display: inline-block;
-      width: 145px;
+      width: 160px;
     }
     label {
       margin-right: 5px;
     }
   `;
 
+  const DropDownComp = styled.div`
+    .dropbtn {
+      background-color: #ffffff;
+      color: rgb(45, 55, 71);
+      padding: 5px;
+      font-size: 14px;
+      border: 1px solid rgb(204, 204, 204);
+      cursor: pointer;
+      width: 100%;
+      text-align: left;
+      border-radius: 4px;
+      &:hover, &:focus {
+        border: 1px solid rgb(55, 187, 155);
+      }
+    }
+
+    .dropdown {
+      position: relative;
+      display: inline-block;
+      width: 100%;
+    }
+
+    .dropdown-content {
+      margin-top: 10px;
+      display: none;
+      position: absolute;
+      background-color: #ffffff;
+      width: 160px;
+      overflow: auto;
+      max-height: 300px;
+      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+      z-index: 1;
+      .dropdown-item {
+        padding: 5px 12px;
+        &:hover {
+          background-color: rgba(55, 187, 155, 0.1);;
+        }
+      }
+    }
+
+    .dropdown-content a {
+      padding: 12px 16px;
+      text-decoration: none;
+      display: block;
+    }
+
+    .dropdown a:hover {background-color: #ddd;}
+
+    .show {display: block;}
+    i {
+      color: rgb(45, 55, 71);
+      border: solid black;
+      border-width: 0 1px 1px 0;
+      display: inline-block;
+      padding: 3px;
+      float: right;
+    }
+    .up {
+      margin-top: 5px;
+      transform: rotate(-135deg);
+      -webkit-transform: rotate(-135deg);
+    }
+
+    .down {
+      margin-top: 3px;
+      transform: rotate(45deg);
+      -webkit-transform: rotate(45deg);
+    }
+  `;
+
   return (
     <ComponentStyle>
-      <Fragment>
-        <label>Versions:</label>
-        <Select
-          valueField="id"
-          placeholder="Documentation"
-          color={colors.green}
-          options={options}
-          values={value}
-          onChange={selectedValue => {
-            if (selectedValue.length > 0 && value !== selectedValue) {
-              let href = selectedValue[0].href,
-                target = "_self";
-              setValue(selectedValue);
-              if (selectedValue[0].title !== "Latest") {
-                href = `http://atlas.apache.org${selectedValue[0].href}`;
-                target = "_blank";
-              }
-              window.open(href, target);
+      <DropDownComp>
+        <div className="dropdown">
+          <button className="dropbtn" onClick={() => setToggle(c => !c)}>Versions <i className={`${toggle ? 'up' : 'down'}`}></i></button>
+          <div ref={dropdownRef} id="myDropdown" className={`dropdown-content ${toggle ? 'show' : ''}`}>
+            {
+              options.map(function(opt, i) {
+                return <div className="dropdown-item" key={i} data-href={opt.href} onClick={
+                  event => {
+                    const dataHref = event.currentTarget.dataset.href;
+                    const href = `${window.location.origin}${dataHref}`;
+                    window.open(href, "_blank");
+                  }}>
+                    {opt.title}
+                </div>
+              })
             }
-          }}
-          {...rest}
-        />
-      </Fragment>
+          </div>
+        </div>
+      </DropDownComp>
     </ComponentStyle>
   );
 };
