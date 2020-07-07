06cd0cb35a285b3c4a7b0b3c662ce7263848f7b6
chaitali borole <chaitali.borole@cloudera.com>
2020-07-07 23:20:19 +0530

    ATLAS-3855 :-  Bulk entity tag association and bulk api enhancement, authorization fix.
    Signed-off-by: nixonrodrigues <nixon@apache.org>

--- server-api/src/main/java/org/apache/atlas/RequestContext.java
+++ server-api/src/main/java/org/apache/atlas/RequestContext.java
@@ -71,6 +71,7 @@ public class RequestContext {
     private boolean     isInNotificationProcessing = false;
     private boolean     isInTypePatching           = false;
     private boolean     createShellEntityForNonExistingReference = false;
+    private boolean     skipFailedEntities = false;
 
     private RequestContext() {
     }
@@ -206,6 +207,14 @@ public class RequestContext {
 
     public void setCreateShellEntityForNonExistingReference(boolean createShellEntityForNonExistingReference) {
         this.createShellEntityForNonExistingReference = createShellEntityForNonExistingReference;
+    }
+
+    public boolean isSkipFailedEntities() {
+        return skipFailedEntities;
+    }
+
+    public void setSkipFailedEntities(boolean skipFailedEntities) {
+        this.skipFailedEntities = skipFailedEntities;
     }
 
     public void recordEntityUpdate(AtlasEntityHeader entity) {
