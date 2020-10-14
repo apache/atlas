3768c9dcd33eb045c834a8e6cebe4703ab0d933d
kevalbhatt <kbhatt@apache.org>
2020-10-14 20:00:28 +0530

    ATLAS-3986: UI Allow user to update the date format from JAVA property file
    (cherry picked from commit 39892854ffb696df70ecbc0f3e965c5a8261f621)

--- dashboardv3/public/js/views/audit/CreateAuditTableLayoutView.js
+++ dashboardv3/public/js/views/audit/CreateAuditTableLayoutView.js
@@ -75,6 +75,13 @@ define(['require',
             createTableWithValues: function(tableDetails) {
                 var attrTable = CommonViewFunction.propertyTable({
                     scope: this,
+                    getValue: function(val, key) {
+                        if (key && key.toLowerCase().indexOf("time") > 0) {
+                            return Utils.formatDate({ date: val });
+                        } else {
+                            return val;
+                        }
+                    },
                     valueObject: tableDetails
                 });
                 return attrTable;
