55cb08102214c8095fb0241d19ee792b035a58c2
nixonrodrigues <nixon@apache.org>
2021-06-10 19:34:50 +0530

    Revert "ATLAS-4058: UI: Fix Entity detail page Hive Column Position with 0 is set to NA."
    This reverts commit de87800c19ea06f53904a2948fcde29a57d2f051.
    
    (cherry picked from commit 1078f25e90134be4a7e9f35f9774c113909aba97)

--- dashboardv2/public/js/views/entity/EntityDetailTableLayoutView.js
+++ dashboardv2/public/js/views/entity/EntityDetailTableLayoutView.js
@@ -81,15 +81,7 @@ define(['require',
                         scope: this,
                         valueObject: _.extend({ "isIncomplete": this.entity.isIncomplete }, this.entity.attributes),
                         attributeDefs: this.attributeDefs,
-                        highlightString: highlightString,
-                        getValue: function(val, key) {
-                            if (key && key.toLowerCase().indexOf("position") === 0 && val === 0) {
-                                //if position value is 0 we are showing N/A
-                                return "N/A";
-                            } else {
-                                return val;
-                            }
-                        }
+                        highlightString: highlightString
                     });
                 this.ui.detailValue.append(table);
                 Utils.togglePropertyRelationshipTableEmptyValues({
