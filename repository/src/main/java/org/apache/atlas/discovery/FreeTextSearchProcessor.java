b9b534286371858f2b004c961aa1f69c00fca69e
Anshul Mehta <anshul.mk97@gmail.com>
2021-10-20 15:29:10 +0530

    fix: minor issue to due to conflict resolving

--- repository/src/main/java/org/apache/atlas/discovery/FreeTextSearchProcessor.java
+++ repository/src/main/java/org/apache/atlas/discovery/FreeTextSearchProcessor.java
@@ -33,10 +33,7 @@ import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 
 /**
