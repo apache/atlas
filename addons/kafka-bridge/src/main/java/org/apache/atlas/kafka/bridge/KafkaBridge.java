7871d9e1a6efe6875b6d4da9326cefa31ea20c0e
Aileen Toleikis <aileen.toleikis@hpe.com>
2021-11-15 11:18:49 -0800

    ATLAS-4246: Make Kafka Interface aware of Kafka Schema Registry
    Signed-off-by: Sidharth Mishra <sidmishra@apache.org>
    (cherry picked from commit b4ee37b6d850baa0803deb39652e6f4ff6d665e7)

--- addons/kafka-bridge/src/main/java/org/apache/atlas/kafka/bridge/KafkaBridge.java
+++ addons/kafka-bridge/src/main/java/org/apache/atlas/kafka/bridge/KafkaBridge.java
@@ -39,6 +39,10 @@ import org.apache.commons.configuration.Configuration;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.security.UserGroupInformation;
 
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,12 +53,15 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Pattern;
+import org.apache.avro.Schema;
+import java.io.IOException;
 
 public class KafkaBridge {
     private static final Logger LOG                               = LoggerFactory.getLogger(KafkaBridge.class);
+    private static final String KAFKA_SCHEMA_REGISTRY_ENV_VARIABLE= System.getenv("KAFKA_SCHEMA_REGISTRY");
+    public static        String KAFKA_SCHEMA_REGISTRY_HOSTNAME    = "localhost";
     private static final int    EXIT_CODE_SUCCESS                 = 0;
     private static final int    EXIT_CODE_FAILED                  = 1;
     private static final String ATLAS_ENDPOINT                    = "atlas.rest.address";
@@ -70,18 +77,31 @@ public class KafkaBridge {
     private static final String URI                               = "uri";
     private static final String CLUSTERNAME                       = "clusterName";
     private static final String TOPIC                             = "topic";
-    private static final String FORMAT_KAFKA_TOPIC_QUALIFIED_NAME = "%s@%s";
+    private static final String FORMAT_KAKFA_TOPIC_QUALIFIED_NAME = "%s@%s";
+    private static final String TYPE                              = "type";
+    private static final String NAMESPACE                         = "namespace";
+    private static final String FIELDS                            = "fields";
+    private static final String AVRO_SCHEMA                       = "avroSchema";
+    private static final String SCHEMA_VERSION_ID                 = "versionId";
+
+    private static final String FORMAT_KAKFA_SCHEMA_QUALIFIED_NAME      = "%s@%s@%s";
+    private static final String FORMAT_KAKFA_FIELD_QUALIFIED_NAME       = "%s@%s@%s@%s";
 
     private final List<String>  availableTopics;
     private final String        metadataNamespace;
     private final AtlasClientV2 atlasClientV2;
     private final KafkaUtils    kafkaUtils;
-
+    private final CloseableHttpClient httpClient;
 
     public static void main(String[] args) {
         int           exitCode      = EXIT_CODE_FAILED;
         AtlasClientV2 atlasClientV2 = null;
         KafkaUtils    kafkaUtils    = null;
+        CloseableHttpClient httpClient = null;
+
+        System.out.print("\n################################\n");
+        System.out.print("# Custom Kafka bridge #\n");
+        System.out.print("################################\n\n");
 
         try {
             Options options = new Options();
@@ -113,6 +133,10 @@ public class KafkaBridge {
             kafkaUtils = new KafkaUtils(atlasConf);
 
             KafkaBridge importer = new KafkaBridge(atlasConf, atlasClientV2, kafkaUtils);
+
+            if(!KAFKA_SCHEMA_REGISTRY_ENV_VARIABLE.isEmpty()){
+                KAFKA_SCHEMA_REGISTRY_HOSTNAME = KAFKA_SCHEMA_REGISTRY_ENV_VARIABLE;
+            }
 
             if (StringUtils.isNotEmpty(fileToImport)) {
                 File f = new File(fileToImport);
@@ -154,7 +178,17 @@ public class KafkaBridge {
             if (kafkaUtils != null) {
                 kafkaUtils.close();
             }
+
+            if (httpClient != null) {
+                try {
+                    httpClient.close();
+                } catch (IOException e) {
+                    LOG.error("Could not close http client: ", e);
+                }
+            }
         }
+
+        System.out.print("\n\n");
 
         System.exit(exitCode);
     }
@@ -164,6 +198,7 @@ public class KafkaBridge {
         this.metadataNamespace = getMetadataNamespace(atlasConf);
         this.kafkaUtils        = kafkaUtils;
         this.availableTopics   = this.kafkaUtils.listAllTopics();
+        this.httpClient        = HttpClientBuilder.create().build();
     }
 
     private String getMetadataNamespace(Configuration config) {
@@ -199,7 +234,9 @@ public class KafkaBridge {
     @VisibleForTesting
     AtlasEntityWithExtInfo createOrUpdateTopic(String topic) throws Exception {
         String                 topicQualifiedName = getTopicQualifiedName(metadataNamespace, topic);
-        AtlasEntityWithExtInfo topicEntity        = findTopicEntityInAtlas(topicQualifiedName);
+        AtlasEntityWithExtInfo topicEntity        = findEntityInAtlas(KafkaDataTypes.KAFKA_TOPIC.getName(),topicQualifiedName);
+
+        System.out.print("\n"); // add a new line for each topic
 
         if (topicEntity == null) {
             System.out.println("Adding Kafka topic " + topic);
@@ -223,8 +260,63 @@ public class KafkaBridge {
     }
 
     @VisibleForTesting
+    AtlasEntityWithExtInfo createOrUpdateSchema(String schema, String schemaName, String namespace, int version) throws Exception {
+        String                 schemaQualifiedName = getSchemaQualifiedName(metadataNamespace, schemaName + "-value", "v" + version);
+        AtlasEntityWithExtInfo schemaEntity        = findEntityInAtlas(KafkaDataTypes.AVRO_SCHEMA.getName(), schemaQualifiedName);
+
+        if (schemaEntity == null) {
+            System.out.println("---Adding Kafka schema " + schema);
+            LOG.info("Importing Kafka schema: {}", schemaQualifiedName);
+
+            AtlasEntity entity = getSchemaEntity(schema, schemaName, namespace, version, null);
+
+            schemaEntity = createEntityInAtlas(new AtlasEntityWithExtInfo(entity));
+        } else {
+            System.out.println("---Updating Kafka schema "  + schema);
+            LOG.info("Kafka schema {} already exists in Atlas. Updating it..", schemaQualifiedName);
+
+            AtlasEntity entity = getSchemaEntity(schema, schemaName, namespace, version, schemaEntity.getEntity());
+
+            schemaEntity.setEntity(entity);
+
+            schemaEntity = updateEntityInAtlas(schemaEntity);
+        }
+
+        return schemaEntity;
+    }
+
+    @VisibleForTesting
+    AtlasEntityWithExtInfo createOrUpdateField(Schema.Field field, String schemaName, String namespace, int version, String fullname) throws Exception {
+        fullname = concatFullname(field.name(), fullname, "");
+        String                 fieldQualifiedName = getFieldQualifiedName(metadataNamespace, fullname, schemaName + "-value", "v" + version);
+        AtlasEntityWithExtInfo fieldEntity        = findEntityInAtlas(KafkaDataTypes.AVRO_FIELD.getName(), fieldQualifiedName);
+
+        if (fieldEntity == null) {
+            System.out.println("---Adding Avro field " + fullname);
+            LOG.info("Importing Avro field: {}", fieldQualifiedName);
+
+            AtlasEntity entity = getFieldEntity(field, schemaName, namespace, version ,null, fullname);
+
+            fieldEntity = createEntityInAtlas(new AtlasEntityWithExtInfo(entity));
+        } else {
+            System.out.println("---Updating Avro field "  + fullname);
+            LOG.info("Avro field {} already exists in Atlas. Updating it..", fieldQualifiedName);
+
+            AtlasEntity entity = getFieldEntity(field, schemaName, namespace, version, fieldEntity.getEntity(), fullname);
+
+            fieldEntity.setEntity(entity);
+
+            fieldEntity = updateEntityInAtlas(fieldEntity);
+        }
+
+        return fieldEntity;
+    }
+
+
+    @VisibleForTesting
     AtlasEntity getTopicEntity(String topic, AtlasEntity topicEntity) throws Exception {
         final AtlasEntity ret;
+        List<AtlasEntity> createdSchemas;
 
         if (topicEntity == null) {
             ret = new AtlasEntity(KafkaDataTypes.KAFKA_TOPIC.getName());
@@ -250,33 +342,140 @@ public class KafkaBridge {
             throw new Exception("Error while getting partition data for topic :" + topic, e);
         }
 
-        return ret;
-    }
+        createdSchemas = findOrCreateAtlasSchema(topic);
 
-    @VisibleForTesting
-    static String getTopicQualifiedName(String metadataNamespace, String topic) {
-        return String.format(FORMAT_KAFKA_TOPIC_QUALIFIED_NAME, topic.toLowerCase(), metadataNamespace);
-    }
-
-    private AtlasEntityWithExtInfo findTopicEntityInAtlas(String topicQualifiedName) {
-        AtlasEntityWithExtInfo ret = null;
-
-        try {
-            ret = findEntityInAtlas(KafkaDataTypes.KAFKA_TOPIC.getName(), topicQualifiedName);
-
-            clearRelationshipAttributes(ret);
-        } catch (Exception e) {
-            ret = null; // entity doesn't exist in Atlas
+        if(createdSchemas.size() > 0) {
+            ret.setAttribute(AVRO_SCHEMA, createdSchemas);
+            ret.setRelationshipAttribute(AVRO_SCHEMA, createdSchemas);
         }
 
         return ret;
     }
 
     @VisibleForTesting
-     AtlasEntityWithExtInfo findEntityInAtlas(String typeName, String qualifiedName) throws Exception {
-        Map<String, String> attributes = Collections.singletonMap(ATTRIBUTE_QUALIFIED_NAME, qualifiedName);
+    AtlasEntity getSchemaEntity(String schema, String schemaName, String namespace, int version, AtlasEntity schemaEntity) throws Exception {
+        final AtlasEntity ret;
+        List<AtlasEntity> createdFields = new ArrayList<>();
 
-        return atlasClientV2.getEntityByAttribute(typeName, attributes);
+
+        if (schemaEntity == null) {
+            ret = new AtlasEntity(KafkaDataTypes.AVRO_SCHEMA.getName());
+        } else {
+            ret = schemaEntity;
+        }
+
+        Schema parsedSchema = new Schema.Parser().parse(schema);
+
+        String qualifiedName = getSchemaQualifiedName(metadataNamespace, schemaName + "-value", "v" + version);
+
+        if (namespace == null) {
+            namespace = (parsedSchema.getNamespace() != null) ? parsedSchema.getNamespace() : KAFKA_METADATA_NAMESPACE;
+        }
+
+        ret.setAttribute(ATTRIBUTE_QUALIFIED_NAME, qualifiedName);
+        ret.setAttribute(TYPE, parsedSchema.getType());
+        ret.setAttribute(NAMESPACE, namespace);
+        ret.setAttribute(NAME,parsedSchema.getName() + "(v" + version + ")");
+        ret.setAttribute(SCHEMA_VERSION_ID, version);
+
+        createdFields = createNestedFields(parsedSchema, schemaName, namespace, version, "");
+
+        if(createdFields.size() > 0) {
+            ret.setRelationshipAttribute(FIELDS, createdFields);
+        }
+
+        return ret;
+    }
+
+    List<AtlasEntity> createNestedFields(Schema parsedSchema, String schemaName, String namespace, int version, String fullname) throws Exception {
+        List<AtlasEntity>  entityArray = new ArrayList<>();
+        AtlasEntityWithExtInfo fieldInAtlas;
+        JSONParser parser = new JSONParser();
+
+        for (Schema.Field field:parsedSchema.getFields()) {
+
+            if(field.schema().getType() == Schema.Type.ARRAY){
+                System.out.println("ARRAY DETECTED");
+                String subfields = ((JSONObject) parser.parse(field.schema().toString())).get("items").toString();
+                Schema parsedSubSchema = new Schema.Parser().parse(subfields);
+
+                fullname = concatFullname(field.name(), fullname, parsedSubSchema.getName());
+
+                entityArray.addAll(createNestedFields(parsedSubSchema, schemaName, namespace, version, fullname));
+            }
+
+            else if(field.schema().getType() == Schema.Type.RECORD && !schemaName.equals(field.name())) {
+                    System.out.println("NESTED RECORD DETECTED");
+                    fullname = concatFullname(field.name(), fullname, "");
+                    entityArray.addAll(createNestedFields(field.schema(), schemaName, namespace, version, fullname));
+            }
+
+            else{
+                fieldInAtlas = createOrUpdateField(field, schemaName, namespace, version, fullname);
+
+                entityArray.add(fieldInAtlas.getEntity());
+            }
+        }
+        entityArray.sort((o1, o2) -> {
+            if (o1.getAttribute(NAME) != null && o2.getAttribute(NAME) != null) {
+                String str1 = o1.getAttribute(NAME).toString();
+                String str2 = o2.getAttribute(NAME).toString();
+
+                return str1.compareTo(str2);
+            } else {
+                return 0;
+            }
+        });
+
+        return entityArray;
+    }
+
+    @VisibleForTesting
+    AtlasEntity getFieldEntity(Schema.Field field, String schemaName, String namespace, int version, AtlasEntity fieldEntity, String fullname) throws Exception {
+        AtlasEntity ret;
+
+        if (fieldEntity == null) {
+            ret = new AtlasEntity(KafkaDataTypes.AVRO_FIELD.getName());
+        } else {
+            ret = fieldEntity;
+        }
+
+        String qualifiedName = getFieldQualifiedName(metadataNamespace, fullname, schemaName + "-value", "v" + version);
+
+        ret.setAttribute(ATTRIBUTE_QUALIFIED_NAME, qualifiedName);
+        ret.setAttribute(NAME,fullname + "(v" + version + ")");
+        //ret.setAttribute(field.schema().getType()); --> does not work, since type expects array<avro_type>. Instead setting Description
+        ret.setAttribute(DESCRIPTION_ATTR, field.schema().getType());
+        return ret;
+    }
+
+    @VisibleForTesting
+    static String getTopicQualifiedName(String metadataNamespace, String topic) {
+        return String.format(FORMAT_KAKFA_TOPIC_QUALIFIED_NAME, topic.toLowerCase(), metadataNamespace);
+    }
+
+    @VisibleForTesting
+    static String getSchemaQualifiedName(String metadataNamespace, String schema, String version) {
+        return String.format(FORMAT_KAKFA_SCHEMA_QUALIFIED_NAME, schema.toLowerCase(), version, metadataNamespace);
+    }
+
+    @VisibleForTesting
+    static String getFieldQualifiedName(String metadataNamespace, String field, String schemaName, String version) {
+        return String.format(FORMAT_KAKFA_FIELD_QUALIFIED_NAME , field.toLowerCase(), schemaName.toLowerCase(), version, metadataNamespace);
+    }
+
+    @VisibleForTesting
+     AtlasEntityWithExtInfo findEntityInAtlas(String typeName, String qualifiedName) throws Exception {
+        AtlasEntityWithExtInfo ret = null;
+
+        try {
+            ret = atlasClientV2.getEntityByAttribute(typeName, Collections.singletonMap(ATTRIBUTE_QUALIFIED_NAME, qualifiedName));
+        }
+        catch (Exception e){
+            LOG.info("Exception on finding Atlas Entity: {}", e);
+        }
+
+        return ret;
     }
 
     @VisibleForTesting
@@ -284,7 +483,6 @@ public class KafkaBridge {
         AtlasEntityWithExtInfo  ret      = null;
         EntityMutationResponse  response = atlasClientV2.createEntity(entity);
         List<AtlasEntityHeader> entities = response.getCreatedEntities();
-
         if (CollectionUtils.isNotEmpty(entities)) {
             AtlasEntityWithExtInfo getByGuidResponse = atlasClientV2.getEntityByGuid(entities.get(0).getGuid());
 
@@ -357,5 +555,67 @@ public class KafkaBridge {
         if (entity != null && entity.getRelationshipAttributes() != null) {
             entity.getRelationshipAttributes().clear();
         }
+    }
+
+    private List<AtlasEntity> findOrCreateAtlasSchema(String schemaName) throws Exception {
+        List<AtlasEntity> entities = new ArrayList<>();
+        // Handling Schemas
+        ArrayList<Integer> versions = SchemaRegistryConnector.getVersionsKafkaSchemaRegistry(httpClient,schemaName + "-value");
+
+        for (int version:versions) {
+            String kafkaSchema = SchemaRegistryConnector.getSchemaFromKafkaSchemaRegistry(httpClient, schemaName + "-value", version);
+
+            if(kafkaSchema != null) {
+                // Schema exists in Kafka Schema Registry
+                System.out.println("---Found Schema " + schemaName + "-value in Kafka Schema Registry with Version " + version);
+                LOG.info("Found Schema {}-value in Kafka Schema Registry with Version {}", schemaName, version);
+
+                AtlasEntityWithExtInfo atlasSchemaEntity = findEntityInAtlas(KafkaDataTypes.AVRO_SCHEMA.getName(), getSchemaQualifiedName(metadataNamespace, schemaName  + "-value", "v" + version));
+
+                if(atlasSchemaEntity != null) {
+                    // Schema exists in Kafka Schema Registry AND in Atlas
+
+                    System.out.println("---Found Entity avro_schema " + schemaName + " in Atlas");
+                    LOG.info("Found Entity avro_schema {} in Atlas", schemaName);
+
+                    AtlasEntityWithExtInfo createdSchema = createOrUpdateSchema(kafkaSchema, schemaName, null, version);
+
+                    entities.add(createdSchema.getEntity());
+                }
+                else {
+                    // Schema exists in Kafka Schema Registry but NOT in Atlas
+                    System.out.println("---NOT Found Entity avro_schema " + schemaName + " in Atlas");
+                    LOG.info("NOT Found Entity avro_schema {} in Atlas", schemaName);
+
+                    AtlasEntityWithExtInfo createdSchema = createOrUpdateSchema(kafkaSchema, schemaName, null, version);
+
+                    entities.add(createdSchema.getEntity());
+                }
+            }
+        }
+
+        return entities;
+    }
+
+    private String concatFullname(String fieldName,String fullname, String subSchemaName){
+        if(fullname.isEmpty()){
+            if(subSchemaName.isEmpty()) {
+                fullname = fieldName;
+            }
+            else {
+                fullname = fieldName + "." + subSchemaName;
+            }
+
+        }
+        else{
+            if(subSchemaName.isEmpty()) {
+                fullname = fullname + "." + fieldName;
+            }
+            else {
+                fullname = fullname + "." + subSchemaName + "." + fieldName;
+            }
+        }
+
+        return fullname;
     }
 }
