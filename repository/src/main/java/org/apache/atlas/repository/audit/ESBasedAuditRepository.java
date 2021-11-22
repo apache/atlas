0ad3712d10136135cca075cdbdae89919f307463
Anshul Mehta <anshul.mk97@gmail.com>
2021-11-22 17:55:53 +0530

    fix: condition propert type audit repository

--- repository/src/main/java/org/apache/atlas/repository/audit/ESBasedAuditRepository.java
+++ repository/src/main/java/org/apache/atlas/repository/audit/ESBasedAuditRepository.java
@@ -55,7 +55,7 @@ import javax.inject.Singleton;
  */
 @Singleton
 @Component
-@ConditionalOnAtlasProperty(property = "atlas.EntityAuditRepositorySearch.impl")
+@ConditionalOnAtlasProperty(property = "atlas.EntityAuditRepository.impl")
 public class ESBasedAuditRepository extends AbstractStorageBasedAuditRepository {
     private static final Logger LOG = LoggerFactory.getLogger(ESBasedAuditRepository.class);
     public static final String INDEX_BACKEND_CONF = "atlas.graph.index.search.hostname";
