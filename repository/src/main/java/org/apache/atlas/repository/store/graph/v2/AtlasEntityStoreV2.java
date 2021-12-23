hdgokani/atlan_metastore

META-2799 : -Set delete handler type in delete entity notification message.

repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasEntityStoreV2.java
@@ -1550,11 +1550,14 @@ public class AtlasEntityStoreV2 implements AtlasEntityStore {
 
         Collection<AtlasVertex> categories = new ArrayList<>();
         Collection<AtlasVertex> other = new ArrayList<>();
+        List categoryEntityGuids = new ArrayList();
+
         MetricRecorder metric = RequestContext.get().startMetricRecord("filterCategoryVertices");
         for (AtlasVertex vertex : deletionCandidates) {
             String typeName = getTypeName(vertex);
             if (ATLAS_GLOSSARY_CATEGORY_ENTITY_TYPE.equals(typeName)) {
                 categories.add(vertex);
+                categoryEntityGuids.add(GraphHelper.getGuid(vertex));
             } else {
                 other.add(vertex);
             }
@@ -1571,6 +1574,13 @@ public class AtlasEntityStoreV2 implements AtlasEntityStore {
         }
 
         for (AtlasEntityHeader entity : req.getDeletedEntities()) {
+            String handler;
+            if (categoryEntityGuids.contains(entity.getGuid())) {
+                handler = "HARD";
+            } else {
+                handler = RequestContext.get().getDeleteType().name();
+            }
+            entity.setHandler(handler);
             response.addEntity(DELETE, entity);
         }
 

webapp/src/main/java/org/apache/atlas/notification/EntityNotificationListenerV2.java
@@ -209,6 +209,7 @@ public class EntityNotificationListenerV2 implements EntityChangeListenerV2 {
         ret.setUpdatedBy(entity.getUpdatedBy());
         ret.setCreateTime(entity.getCreateTime());
         ret.setUpdateTime(entity.getUpdateTime());
+        ret.setHandler(entity.getHandler());
 
         setAttribute(ret, NAME, name);
         setAttribute(ret, DESCRIPTION, entity.getAttribute(DESCRIPTION));

apache/atlas

(cherry picked from commit a8fb321d00977659a56e01f1e900be0e409971f5)

repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasEntityStoreV2.java
@@ -1570,6 +1570,7 @@ public class AtlasEntityStoreV2 implements AtlasEntityStore {
 
         Collection<AtlasVertex> categories = new ArrayList<>();
         Collection<AtlasVertex> other = new ArrayList<>();
+
         MetricRecorder metric = RequestContext.get().startMetricRecord("filterCategoryVertices");
         for (AtlasVertex vertex : deletionCandidates) {
             String typeName = getTypeName(vertex);
@@ -1591,6 +1592,14 @@ public class AtlasEntityStoreV2 implements AtlasEntityStore {
         }
 
         for (AtlasEntityHeader entity : req.getDeletedEntities()) {
+            String handler;
+            if (ATLAS_GLOSSARY_CATEGORY_ENTITY_TYPE.contains(entity.getTypeName())) {
+                handler = "HARD";
+            } else {
+                handler = RequestContext.get().getDeleteType().name();
+            }
+            entity.setDeleteHandler(handler);
+            entity.setStatus(Status.DELETED);
             response.addEntity(DELETE, entity);
         }
 

webapp/src/main/java/org/apache/atlas/notification/EntityNotificationListenerV2.java
@@ -209,6 +209,7 @@ public class EntityNotificationListenerV2 implements EntityChangeListenerV2 {
         ret.setUpdatedBy(entity.getUpdatedBy());
         ret.setCreateTime(entity.getCreateTime());
         ret.setUpdateTime(entity.getUpdateTime());
+        ret.setDeleteHandler(entity.getDeleteHandler());
 
         setAttribute(ret, NAME, name);
         setAttribute(ret, DESCRIPTION, entity.getAttribute(DESCRIPTION));