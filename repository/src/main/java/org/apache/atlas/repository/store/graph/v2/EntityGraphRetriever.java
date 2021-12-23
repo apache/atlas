b9c8b70d83f32339fcf84e57b0047cdc93479749
Nikhil P Bonte <nikhil.bonte@atlan.com>
2021-12-23 18:49:09 +0530

    Revert logging

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphRetriever.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphRetriever.java
@@ -43,7 +43,11 @@ import org.apache.atlas.model.typedef.AtlasRelationshipEndDef;
 import org.apache.atlas.model.typedef.AtlasStructDef.AtlasAttributeDef;
 import org.apache.atlas.repository.Constants;
 import org.apache.atlas.repository.graph.GraphHelper;
-import org.apache.atlas.repository.graphdb.*;
+import org.apache.atlas.repository.graphdb.AtlasEdge;
+import org.apache.atlas.repository.graphdb.AtlasEdgeDirection;
+import org.apache.atlas.repository.graphdb.AtlasElement;
+import org.apache.atlas.repository.graphdb.AtlasGraph;
+import org.apache.atlas.repository.graphdb.AtlasVertex;
 import org.apache.atlas.type.AtlasArrayType;
 import org.apache.atlas.type.AtlasBuiltInTypes.AtlasObjectIdType;
 import org.apache.atlas.type.AtlasEntityType;
@@ -891,23 +895,6 @@ public class EntityGraphRetriever {
 
     public List<AtlasClassification> getAllClassifications(AtlasVertex entityVertex) throws AtlasBaseException {
         List<AtlasClassification> ret   = new ArrayList<>();
-        LOG.info("entityVertex == null -> {}", entityVertex == null);
-
-        LOG.info( "vertex : {}", GraphHelper.vertexString(entityVertex));
-
-        AtlasVertexQuery query = entityVertex.query();
-        LOG.info("query == null -> {}", query == null);
-
-        query = query.direction(AtlasEdgeDirection.OUT);
-        LOG.info("query == null -> {}", query == null);
-
-
-        query = query.label(CLASSIFICATION_LABEL);
-        LOG.info("query == null -> {}", query == null);
-
-
-        query.edges();
-
         Iterable                  edges = entityVertex.query().direction(AtlasEdgeDirection.OUT).label(CLASSIFICATION_LABEL).edges();
 
         if (edges != null) {
