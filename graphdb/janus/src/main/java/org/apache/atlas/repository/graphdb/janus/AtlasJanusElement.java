9e96de035f26c2da76d8ce793af20e33b0b7c3e6
Nikhil P Bonte <nikhil.bonte@atlan.com>
2022-02-25 11:26:07 +0530

    Migration import changes: Fix replicatedFrom,To attr query issue

--- graphdb/janus/src/main/java/org/apache/atlas/repository/graphdb/janus/AtlasJanusElement.java
+++ graphdb/janus/src/main/java/org/apache/atlas/repository/graphdb/janus/AtlasJanusElement.java
@@ -17,13 +17,7 @@
  */
 package org.apache.atlas.repository.graphdb.janus;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 import org.apache.atlas.repository.graphdb.AtlasEdge;
 import org.apache.atlas.repository.graphdb.AtlasElement;
@@ -52,13 +46,23 @@ public class AtlasJanusElement<T extends Element> implements AtlasElement {
     private T element;
     protected AtlasJanusGraph graph;
 
+    //excludeProperties: Getting key related issue while Migration mode when fetching few attributes from graph
+    //This is dirty fix to ignore getting such attributes value from graph & return null explicitly
+    private Set<String> excludeProperties = new HashSet<>();
+
     public AtlasJanusElement(AtlasJanusGraph graph, T element) {
         this.element = element;
         this.graph = graph;
+
+        excludeProperties.add("replicatedTo");
+        excludeProperties.add("replicatedFrom");
     }
 
     @Override
     public <T> T getProperty(String propertyName, Class<T> clazz) {
+        if (excludeProperties.contains(propertyName)) {
+            return null;
+        }
 
         //add explicit logic to return null if the property does not exist
         //This is the behavior Atlas expects.  Janus throws an exception
