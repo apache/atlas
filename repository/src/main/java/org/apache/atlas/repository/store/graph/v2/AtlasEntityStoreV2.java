b23edaca05a98a5aae209286c7ef939fb00acdcd
Nikhil P Bonte <nikhil.bonte@atlan.com>
2022-03-14 16:31:49 +0530

    META-2862 Glossary term linking to asset enforces update asset check

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasEntityStoreV2.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasEntityStoreV2.java
@@ -1388,11 +1388,34 @@ public class AtlasEntityStoreV2 implements AtlasEntityStore {
                     for (AtlasEntity entity : context.getUpdatedEntities()) {
                         AtlasEntityHeader entityHeaderWithClassifications = entityRetriever.toAtlasEntityHeaderWithClassifications(entity.getGuid());
                         AtlasEntityHeader entityHeader = new AtlasEntityHeader(entity);
+
                         if(CollectionUtils.isNotEmpty(entityHeaderWithClassifications.getClassifications())) {
                             entityHeader.setClassifications(entityHeaderWithClassifications.getClassifications());
                         }
-                        AtlasAuthorizationUtils.verifyAccess(new AtlasEntityAccessRequest(typeRegistry, AtlasPrivilege.ENTITY_UPDATE, entityHeader),
-                                "update entity: type=", entity.getTypeName());
+
+                        AtlasEntityAccessRequest accessRequest = new AtlasEntityAccessRequest(typeRegistry, AtlasPrivilege.ENTITY_UPDATE, new AtlasEntityHeader(entity));
+                        try {
+                            AtlasAuthorizationUtils.verifyAccess(accessRequest, "update entity: type=", entity.getTypeName());
+                        } catch (AtlasBaseException ae) {
+                            if (ae.getAtlasErrorCode() == AtlasErrorCode.UNAUTHORIZED_ACCESS) {
+                                AtlasVertex storedVertex = context.getVertex(entity.getGuid());
+                                AtlasEntity diffEntity = entityComparator.getDiffResult(entity, storedVertex, false).getDiffEntity();
+
+                                if (MapUtils.isEmpty(diffEntity.getAttributes()) &&
+                                    MapUtils.isEmpty(diffEntity.getCustomAttributes()) &&
+                                    MapUtils.isEmpty(diffEntity.getBusinessAttributes()) &&
+                                    CollectionUtils.isEmpty(diffEntity.getClassifications()) &&
+                                    CollectionUtils.isEmpty(diffEntity.getLabels()) &&
+                                    MapUtils.isNotEmpty(diffEntity.getRelationshipAttributes()) &&
+                                    diffEntity.getRelationshipAttributes().size() == 1 &&
+                                    diffEntity.getRelationshipAttributes().containsKey("meanings")) {
+                                    //only diff is relationshipAttributes.meanings, allow update
+                                    LOG.info("Allowing Update");
+                                } else {
+                                    throw ae;
+                                }
+                            }
+                        }
                     }
                 }
 
