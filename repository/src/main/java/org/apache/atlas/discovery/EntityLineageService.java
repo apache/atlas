850c1d5eca20258cef578fd915cfb8c99943eeee
Fatih <fdogmus06@gmail.com>
2022-09-26 21:53:01 +0300

    META-3036 Try fixing the cyclic dependency

--- repository/src/main/java/org/apache/atlas/discovery/EntityLineageService.java
+++ repository/src/main/java/org/apache/atlas/discovery/EntityLineageService.java
@@ -470,6 +470,10 @@ public class EntityLineageService implements AtlasLineageService {
                     if (entityVertex == null) {
                         continue;
                     }
+                    if (getGuid(entityVertex).equals(lineageContext.getGuid())) {
+                        currentOffset++;
+                        continue;
+                    }
                     if (shouldTerminate(isInput, ret, lineageContext, currentVertexEdges, inputVertexCount, i, edgesOfProcess, j)) {
                         return;
                     }
