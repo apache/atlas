739afc740a317a0420d1b8e8ebeb333ee551564e
Nikhil P Bonte <nikhil.bonte@atlan.com>
2022-11-08 20:05:03 +0530

    META-2984 Search logs

--- intg/src/main/java/org/apache/atlas/AtlasConfiguration.java
+++ intg/src/main/java/org/apache/atlas/AtlasConfiguration.java
@@ -86,8 +86,9 @@ public enum AtlasConfiguration {
     TASKS_QUEUE_SIZE("atlas.tasks.queue.size", 1000),
     SESSION_TIMEOUT_SECS("atlas.session.timeout.secs", -1),
     UPDATE_COMPOSITE_INDEX_STATUS("atlas.update.composite.index.status", true),
-    TASKS_GRAPH_COMMIT_CHUNK_SIZE("atlas.tasks.graph.commit.chunk.size", 1000),
-    MAX_NUMBER_OF_RETRIES("atlas.tasks.graph.retry.count", 3);
+    TASKS_GRAPH_COMMIT_CHUNK_SIZE("atlas.tasks.graph.commit.chunk.size", 30),
+    MAX_NUMBER_OF_RETRIES("atlas.tasks.graph.retry.count", 3),
+    ENABLE_SEARCH_LOGGING("atlas.enable.search.logging", true);
 
 
     private static final Configuration APPLICATION_PROPERTIES;
