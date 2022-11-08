6490db743c148a6fbb57ef6c114d1584eb0cf0ed
Madhan Neethiraj <madhan@apache.org>
2022-11-08 10:38:13 -0800

    ATLAS-4709: Python client 0.0.12 release
    (cherry picked from commit d40c1b09f533bce6fd27e97489885c10887fe4ec)

--- intg/src/main/python/setup.py
+++ intg/src/main/python/setup.py
@@ -28,7 +28,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='apache-atlas',
-    version='0.0.11',
+    version='0.0.12',
     author="Apache Atlas",
     author_email='dev@atlas.apache.org',
     description="Apache Atlas Python Client",
