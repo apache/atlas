cd2000edd7a50bdad187d1a3dcb1f97455f7e93f
Suman Das <59254445+sumandas0@users.noreply.github.com>
2022-11-14 12:44:47 +0530

    Added back propagated vertices for backward compatibility

--- repository/src/main/java/org/apache/atlas/repository/graph/GraphHelper.java
+++ repository/src/main/java/org/apache/atlas/repository/graph/GraphHelper.java
@@ -456,20 +456,34 @@ public final class GraphHelper {
         return ret;
     }
 
-    public static List<String> getPropagatedVerticesIds (AtlasVertex classificationVertex) {
-        List<String>   ret      =  new ArrayList<>();
+    public static List<AtlasVertex> getPropagatedVertices (AtlasVertex classificationVertex) {
+        List<AtlasVertex>   ret      =  new ArrayList<AtlasVertex>();
         Iterator<AtlasVertex>            vertices =  classificationVertex.query().direction(AtlasEdgeDirection.IN).label(CLASSIFICATION_LABEL)
                                                             .has(CLASSIFICATION_EDGE_IS_PROPAGATED_PROPERTY_KEY, true)
                                                             .has(CLASSIFICATION_EDGE_NAME_PROPERTY_KEY, getTypeName(classificationVertex))
                                                             .vertices().iterator();
 
         if (vertices != null) {
-           while(vertices.hasNext()){
-               AtlasVertex vertex = vertices.next();
-               if(vertex != null) {
-                   ret.add(vertex.getIdForDisplay());
-               }
-           }
+           ret = IteratorUtils.toList(vertices);
+        }
+
+        return ret;
+    }
+
+    public static List<String> getPropagatedVerticesIds (AtlasVertex classificationVertex) {
+        List<String>   ret      =  new ArrayList<>();
+        Iterator<AtlasVertex>            vertices =  classificationVertex.query().direction(AtlasEdgeDirection.IN).label(CLASSIFICATION_LABEL)
+                .has(CLASSIFICATION_EDGE_IS_PROPAGATED_PROPERTY_KEY, true)
+                .has(CLASSIFICATION_EDGE_NAME_PROPERTY_KEY, getTypeName(classificationVertex))
+                .vertices().iterator();
+
+        if (vertices != null) {
+            while (vertices.hasNext()) {
+                AtlasVertex vertex = vertices.next();
+                if (vertex != null) {
+                    ret.add(vertex.getIdForDisplay());
+                }
+            }
         }
 
         return ret;
