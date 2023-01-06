1684df5e7843717026ec3bd9236e75e16aa618ac
Suman Das <59254445+sumandas0@users.noreply.github.com>
2023-01-06 20:11:56 +0530

    @sumandas0 fix: refactoring code to have better naming #2

--- repository/src/main/java/org/apache/atlas/tasks/TaskManagement.java
+++ repository/src/main/java/org/apache/atlas/tasks/TaskManagement.java
@@ -130,8 +130,8 @@ public class TaskManagement implements Service, ActiveStateChangeHandler {
         createTaskTypeFactoryMap(this.taskTypeFactoryMap, taskFactory);
     }
 
-    public AtlasTask createTask(String taskType, String createdBy, Map<String, Object> parameters, String classificationId, String entityId) {
-        return this.registry.createVertex(taskType, createdBy, parameters, classificationId, entityId);
+    public AtlasTask createTask(String taskType, String createdBy, Map<String, Object> parameters, String classificationId, String entityGuid) {
+        return this.registry.createVertex(taskType, createdBy, parameters, classificationId, entityGuid);
     }
 
     public List<AtlasTask> getAll() {
