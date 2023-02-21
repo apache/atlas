9076027ce5a9c70ca507c17afab3821a4b88357e
Suman Das <59254445+sumandas0@users.noreply.github.com>
2023-02-21 17:15:49 +0530

    refactored few unwanted commits

--- repository/src/main/java/org/apache/atlas/repository/audit/EntityAuditListenerV2.java
+++ repository/src/main/java/org/apache/atlas/repository/audit/EntityAuditListenerV2.java
@@ -607,7 +607,6 @@ public class EntityAuditListenerV2 implements EntityChangeListenerV2 {
         StringBuilder sb = new StringBuilder();
 
         if (action == ENTITY_DELETE || action == ENTITY_IMPORT_DELETE || action == ENTITY_PURGE) {
-
             Map<String, Object> attrValues = new HashMap<>(entity.getAttributes());
             entity.setAttributes(null);
             for (AtlasAttribute attribute : entityType.getUniqAttributes().values()) {
