79431ec20fc873f175f64c0972dbf497a7d16ccc
Suman Das <59254445+sumandas0@users.noreply.github.com>
2023-02-23 09:29:16 +0530

    refactor: Use existing available constants

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/PreProcessorUtils.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/PreProcessorUtils.java
@@ -4,8 +4,9 @@ import org.apache.atlas.repository.graphdb.AtlasVertex;
 import org.apache.atlas.util.NanoIdUtils;
 import org.apache.commons.lang.StringUtils;
 
-import static org.apache.atlas.repository.Constants.ENTITY_TYPE_PROPERTY_KEY;
+import static org.apache.atlas.repository.Constants.QUERY_COLLECTION_ENTITY_TYPE;
 import static org.apache.atlas.repository.Constants.QUALIFIED_NAME;
+import static org.apache.atlas.repository.Constants.ENTITY_TYPE_PROPERTY_KEY;
 
 public class PreProcessorUtils {
 
@@ -18,7 +19,6 @@ public class PreProcessorUtils {
 
     //Query models constants
     public static final String PREFIX_QUERY_QN   = "default/collection/";
-    public static final String TYPE_COLLECTION = "Collection";
     public static final String COLLECTION_QUALIFIED_NAME = "collectionQualifiedName";
     public static final String PARENT_QUALIFIED_NAME = "parentQualifiedName";
     public static final String PARENT_ATTRIBUTE_NAME    = "parent";
@@ -43,6 +43,6 @@ public class PreProcessorUtils {
     }
 
     public static String getCollectionPropertyName(AtlasVertex parentVertex) {
-        return TYPE_COLLECTION.equals(parentVertex.getProperty(ENTITY_TYPE_PROPERTY_KEY, String.class)) ? QUALIFIED_NAME : COLLECTION_QUALIFIED_NAME;
+        return QUERY_COLLECTION_ENTITY_TYPE.equals(parentVertex.getProperty(ENTITY_TYPE_PROPERTY_KEY, String.class)) ? QUALIFIED_NAME : COLLECTION_QUALIFIED_NAME;
     }
 }
