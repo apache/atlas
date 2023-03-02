0a9a3a33cc96435ebb20997d81276942eabe2de6
Suman Das <59254445+sumandas0@users.noreply.github.com>
2023-03-02 19:41:09 +0530

    fix: use owasp formatter to deal with query issue

--- webapp/src/main/java/org/apache/atlas/web/filters/AtlasXSSPreventionFilter.java
+++ webapp/src/main/java/org/apache/atlas/web/filters/AtlasXSSPreventionFilter.java
@@ -1,12 +1,12 @@
 package org.apache.atlas.web.filters;
 
 import org.apache.atlas.AtlasConfiguration;
-import org.apache.atlas.RequestContext;
-import org.apache.atlas.utils.AtlasPerfMetrics;
 import org.apache.atlas.web.util.CachedBodyHttpServletRequest;
 import org.apache.commons.io.IOUtils;
-import org.jsoup.safety.Safelist;
-import org.jsoup.Jsoup;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.owasp.html.HtmlPolicyBuilder;
+import org.owasp.html.PolicyFactory;
+import org.owasp.html.Sanitizers;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -28,6 +28,7 @@ public class AtlasXSSPreventionFilter implements Filter {
     private static final String MASK_STRING = "##ATLAN##";
     private static final String CONTENT_TYPE_JSON = "application/json";
     private static final String ERROR_INVALID_CHARACTERS = "invalid characters in the request body (XSS Filter)";
+    private PolicyFactory policy;
 
     @Inject
     public AtlasXSSPreventionFilter() throws ServletException {
@@ -43,6 +44,14 @@ public class AtlasXSSPreventionFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         maskPattern     = Pattern.compile(AtlasConfiguration.REST_API_XSS_FILTER_MASK_STRING.getString());
+        policy          = new HtmlPolicyBuilder()
+                                .toFactory()
+                                .and(Sanitizers.TABLES)
+                                .and(Sanitizers.FORMATTING)
+                                .and(Sanitizers.BLOCKS)
+                                .and(Sanitizers.IMAGES)
+                                .and(Sanitizers.LINKS)
+                                .and(Sanitizers.STYLES);
 
     }
 
@@ -72,11 +81,11 @@ public class AtlasXSSPreventionFilter implements Filter {
 
         CachedBodyHttpServletRequest cachedBodyHttpServletRequest = new CachedBodyHttpServletRequest(request);
         String body = IOUtils.toString(cachedBodyHttpServletRequest.getInputStream(), "UTF-8");
-        String reqBodyStr = maskPattern.matcher(body).replaceAll(MASK_STRING);
-        Safelist safelist = Safelist.basicWithImages();
+        String reqBodyStr = StringEscapeUtils.unescapeJava(maskPattern.matcher(body).replaceAll(MASK_STRING));
+        String sanitizedBody = policy.sanitize(reqBodyStr);
 
-        if(!Jsoup.isValid(reqBodyStr, safelist)) {
-            response.setHeader("Content-Type", "application/json");
+        if(!StringUtils.equals(reqBodyStr, StringEscapeUtils.unescapeHtml4(sanitizedBody))) {
+            response.setHeader("Content-Type", CONTENT_TYPE_JSON);
             response.setStatus(400);
             response.getWriter().write(getErrorMessages(ERROR_INVALID_CHARACTERS));
             return;
