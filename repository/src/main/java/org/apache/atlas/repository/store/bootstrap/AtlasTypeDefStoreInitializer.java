1cd19e41a9946d98692b8aaa09bdb638d8a22eb1
Nikhil P Bonte <nikhil.bonte@atlan.com>
2023-03-22 23:33:13 +0530

    META-4203 Policy transformer for caching
    (cherry picked from commit 0060a850774d506117e5b8d8c204a1c4358fbde1)

--- repository/src/main/java/org/apache/atlas/repository/store/bootstrap/AtlasTypeDefStoreInitializer.java
+++ repository/src/main/java/org/apache/atlas/repository/store/bootstrap/AtlasTypeDefStoreInitializer.java
@@ -383,7 +383,7 @@ public class AtlasTypeDefStoreInitializer implements ActiveStateChangeHandler {
             loadBootstrapTypeDefs();
             typeDefStore.notifyLoadCompletion();
             try {
-                AtlasAuthorizerFactory.getAtlasAuthorizer();
+                AtlasAuthorizerFactory.getAtlasAuthorizer(typeRegistry);
             } catch (Throwable t) {
                 LOG.error("AtlasTypeDefStoreInitializer.instanceIsActive(): Unable to obtain AtlasAuthorizer", t);
             }
