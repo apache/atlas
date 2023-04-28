ab53ad4e3dcbcaf6239dae0dd1e705f5c962fdb2
suraj5077 <suraj.gupta@atlan.com>
2023-04-28 17:09:46 +0530

    comments

--- repository/src/main/java/org/apache/atlas/discovery/EntityLineageService.java
+++ repository/src/main/java/org/apache/atlas/discovery/EntityLineageService.java
@@ -470,11 +470,11 @@ public class EntityLineageService implements AtlasLineageService {
                 AtlasVertex processVertex = AtlasGraphUtilsV2.findByGuid(this.graph, currentContext.getProcessGUID());
                 AtlasVertex currentVertex = AtlasGraphUtilsV2.findByGuid(this.graph, currentContext.getVertexGUID());
 
-                if (skippedVertices.contains(getGuid(currentVertex)))   // Skipped vertices due to offset should not be processed if visited again via cyclic route
+                if (skippedVertices.contains(getGuid(currentVertex)))   // Skipped vertices due to offset should not be processed if visited again via any cyclic path
                     continue;
 
                 if (visitedVertices.contains(currentContext.getVertexGUID())) {
-                    appendProcessToResult(processVertex, lineageListContext, ret);  // If a visited entity is reached again, but via different path then we include the new Process in response
+                    appendProcessToResult(processVertex, lineageListContext, ret);  // If a visited entity is reached again, but via different path we only include the new Process in response and don't visit it again
                     continue;
                 }
 
