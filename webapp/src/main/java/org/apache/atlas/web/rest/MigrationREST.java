e722b003a0183f9502f8790db73a623778d532c6
Suman Das <59254445+sumandas0@users.noreply.github.com>
2023-05-10 09:35:00 +0530

    feat: Upsert for connections and collections
    (cherry picked from commit f33ed2eb60982cc2d8faf65b53b6f39a2065933e)

--- webapp/src/main/java/org/apache/atlas/web/rest/MigrationREST.java
+++ webapp/src/main/java/org/apache/atlas/web/rest/MigrationREST.java
@@ -1,12 +1,11 @@
 package org.apache.atlas.web.rest;
 
 import javax.ws.rs.Path;
-
 import org.apache.atlas.AtlasErrorCode;
 import org.apache.atlas.RequestContext;
 import org.apache.atlas.annotation.Timed;
-import org.apache.atlas.discovery.EntityDiscoveryService;
 import org.apache.atlas.exception.AtlasBaseException;
+import org.apache.atlas.keycloak.client.KeycloakClient;
 import org.apache.atlas.model.discovery.IndexSearchParams;
 import org.apache.atlas.model.instance.AtlasEntity;
 import org.apache.atlas.model.instance.EntityMutationResponse;
@@ -22,7 +21,10 @@ import org.apache.atlas.v1.model.instance.Id;
 import org.apache.atlas.web.util.Servlets;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
+import org.keycloak.admin.client.resource.*;
+import org.keycloak.representations.idm.GroupRepresentation;
 import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -31,16 +33,9 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
+import java.util.*;
+import java.util.stream.Collectors;
 import static org.apache.atlas.repository.Constants.*;
-import static org.apache.atlas.web.rest.EntityREST.validateAttributeLength;
 
 @Path("migration")
 @Singleton
@@ -51,16 +46,14 @@ public class MigrationREST {
     private static final Logger LOG      = LoggerFactory.getLogger(MigrationREST.class);
     private static final Logger PERF_LOG = AtlasPerfTracer.getPerfLogger("rest.MigrationREST");
 
-    private static final String COLL_ADMIN_ROLE_PATTERN  = "collection_admins_%s";
+    private static final String COLL_ADMIN_ROLE_PATTERN = "collection_admins_%s";
     private static final String COLL_VIEWER_ROLE_PATTERN = "collection_viewer_%s";
-
     public static final String CONN_NAME_PATTERN = "connection_admins_%s";
 
     private final AtlasEntityStore entityStore;
     private final PreProcessorPoliciesTransformer transformer;
     private KeycloakStore keycloakStore;
     private AtlasGraph graph;
-    private EntityDiscoveryService discoveryService;
 
     @Inject
     public MigrationREST(AtlasEntityStore entityStore, AtlasGraph graph) {
@@ -99,11 +92,21 @@ public class MigrationREST {
                         adminUsers.add(entity.getCreatedBy());
                     }
 
+                    if (CollectionUtils.isEmpty(adminGroups)) {
+                        adminGroups = new ArrayList<>();
+                    }
+
+                    if (CollectionUtils.isEmpty(adminRoles)) {
+                        adminRoles = new ArrayList<>();
+                    }
+
                     entity.setAttribute(ATTR_ADMIN_USERS, adminUsers);
 
                     RoleRepresentation role = keycloakStore.getRole(roleName);
                     if (role == null) {
-                        role = keycloakStore.createRoleForConnection(roleName, true, adminUsers, adminGroups, adminRoles);
+                        createCompositeRole(roleName, adminUsers, adminGroups, adminRoles);
+                    }else {
+                        updateCompositeRole(role, adminUsers, adminGroups, adminRoles);
                     }
                     AtlasEntity.AtlasEntitiesWithExtInfo policiesExtInfo = transformer.transform(entity);
                     try {
@@ -131,6 +134,7 @@ public class MigrationREST {
         AtlasPerfTracer perf = null;
         EntityMutationResponse response = new EntityMutationResponse();
         try {
+
             for (AtlasEntity entity : entities.getEntities()) {
                 if (entity.getTypeName().equalsIgnoreCase(QUERY_COLLECTION_ENTITY_TYPE)) {
                     createCollectionAdminRole(entity);
@@ -297,15 +301,27 @@ public class MigrationREST {
         if (adminUsers == null) {
             adminUsers = new ArrayList<>();
         }
+
         if (StringUtils.isNotEmpty(collection.getCreatedBy())) {
             adminUsers.add(collection.getCreatedBy());
+        }
+
+        if (adminGroups == null) {
+            adminGroups = new ArrayList<>();
+        }
+
+        if (adminRoles == null) {
+            adminRoles = new ArrayList<>();
         }
 
         String adminRoleName = String.format(COLL_ADMIN_ROLE_PATTERN, collection.getGuid());
         RoleRepresentation role = keycloakStore.getRole(adminRoleName);
         if (role == null) {
-            role = keycloakStore.createRoleForConnection(adminRoleName, true, adminUsers, adminGroups, adminRoles);
+            createCompositeRole(adminRoleName, adminUsers, adminGroups, adminRoles);
+        } else {
+            updateCompositeRole(role, adminUsers, adminGroups, adminRoles);
         }
+
 
         return role;
     }
@@ -315,10 +331,166 @@ public class MigrationREST {
         String viewerRoleName = String.format(COLL_VIEWER_ROLE_PATTERN, collection.getGuid());
         List<String> viewerUsers = (List<String>) collection.getAttribute(ATTR_VIEWER_USERS);
         List<String> viewerGroups = (List<String>) collection.getAttribute(ATTR_VIEWER_GROUPS);
+
+        if (viewerUsers == null) {
+            viewerUsers = new ArrayList<>();
+        }
+
+        if (viewerGroups == null) {
+            viewerGroups = new ArrayList<>();
+        }
+
         RoleRepresentation role = keycloakStore.getRole(viewerRoleName);
         if (role == null) {
-            role = keycloakStore.createRoleForConnection(viewerRoleName, true, viewerUsers, viewerGroups, null);
+            createCompositeRole(viewerRoleName, viewerUsers, viewerGroups, new ArrayList<>());
+        } else {
+            updateCompositeRole(role, viewerUsers, viewerGroups, new ArrayList<>());
         }
         return role;
+    }
+
+    private void updateCompositeRole(RoleRepresentation role, List<String> users, List<String> groups, List<String> roles) throws AtlasBaseException {
+        UsersResource usersResource = KeycloakClient.getKeycloakClient().getRealm().users();
+        GroupsResource groupsResource = KeycloakClient.getKeycloakClient().getRealm().groups();
+        RolesResource rolesResource      = KeycloakClient.getKeycloakClient().getRealm().roles();
+        RoleByIdResource rolesIdResource = KeycloakClient.getKeycloakClient().getRealm().rolesById();
+
+        RoleResource connectionRoleResource = rolesResource.get(role.getName());
+        List<UserRepresentation> currentUsers = connectionRoleResource.getRoleUserMembers().stream().collect(Collectors.toList());
+        List<GroupRepresentation> currentGroups = connectionRoleResource.getRoleGroupMembers().stream().collect(Collectors.toList());
+        List<RoleRepresentation> currentRoles = connectionRoleResource.getRoleComposites().stream().collect(Collectors.toList());
+
+        //Find users to add and remove from role, if the user is already in the role, don't add it again
+        List<String> usersToAdd = users.stream().filter(user -> currentUsers.stream().noneMatch(userRep -> userRep.getUsername().equals(user))).collect(Collectors.toList());
+        List<UserRepresentation> usersToRemove = currentUsers.stream().filter(user -> users.stream().noneMatch(userRep -> userRep.equals(user.getUsername()))).collect(Collectors.toList());
+
+        //Find groups to add and remove from role, if the group is already in the role, don't add it again
+        List<String> groupsToAdd = groups.stream().filter(group -> currentGroups.stream().noneMatch(groupRep -> groupRep.getName().equals(group))).collect(Collectors.toList());
+        List<GroupRepresentation> groupsToRemove = currentGroups.stream().filter(group -> groups.stream().noneMatch(groupRep -> groupRep.equals(group.getName()))).collect(Collectors.toList());
+
+        //Find roles to add and remove from role, if the role is already in the role, don't add it again
+        List<String> rolesToAdd = roles.stream().filter(roleToAdd -> currentRoles.stream().noneMatch(roleRep -> roleRep.getId().equals(roleToAdd))).collect(Collectors.toList());
+        List<RoleRepresentation> rolesToRemove = currentRoles.stream().filter(roleToRemove -> roles.stream().noneMatch(roleRep -> roleRep.equals(roleToRemove.getId()))).collect(Collectors.toList());
+
+        //Add users to role
+        for (String userName : usersToAdd) {
+            List<UserRepresentation> matchedUsers = usersResource.search(userName);
+            Optional<UserRepresentation> keyUserOptional = matchedUsers.stream().filter(x -> userName.equals(x.getUsername())).findFirst();
+            if (keyUserOptional.isPresent()) {
+                UserRepresentation keyUser = keyUserOptional.get();
+                UserResource userResource = usersResource.get(keyUser.getId());
+                userResource.roles().realmLevel().add(Collections.singletonList(role));
+                userResource.update(keyUser);
+            }
+        }
+        //Remove users from role
+        for (UserRepresentation userToRemove : usersToRemove) {
+            UserResource userResource = usersResource.get(userToRemove.getId());
+            userResource.roles().realmLevel().remove(Collections.singletonList(role));
+            userResource.update(userToRemove);
+        }
+
+        //Add groups to role
+        for (String groupName : groupsToAdd) {
+            List<GroupRepresentation> matchedGroups = groupsResource.groups(groupName, 0, 1);
+            Optional<GroupRepresentation> keyGroupOptional = matchedGroups.stream().filter(x -> groupName.equals(x.getName())).findFirst();
+            if (keyGroupOptional.isPresent()) {
+                GroupRepresentation keyGroup = keyGroupOptional.get();
+                GroupResource groupResource = groupsResource.group(keyGroup.getId());
+                groupResource.roles().realmLevel().add(Collections.singletonList(role));
+                groupResource.update(keyGroup);
+            }
+        }
+        //Remove groups from role
+        for (GroupRepresentation groupToRemove : groupsToRemove) {
+            GroupResource groupResource = groupsResource.group(groupToRemove.getId());
+            groupResource.roles().realmLevel().remove(Collections.singletonList(role));
+            groupResource.update(groupToRemove);
+        }
+
+        //Add roles to role
+        for (String roleId : rolesToAdd) {
+            RoleRepresentation roleToAdd = rolesIdResource.getRole(roleId);
+            connectionRoleResource.addComposites(Collections.singletonList(roleToAdd));
+        }
+
+        //Remove roles from role
+        for (RoleRepresentation roleToRemove : rolesToRemove) {
+            connectionRoleResource.deleteComposites(Collections.singletonList(roleToRemove));
+        }
+    }
+
+    private void createCompositeRole(String roleName, List<String> users, List<String> groups, List<String> roles) throws AtlasBaseException {
+        List<UserRepresentation> roleUsers = new ArrayList<>();
+        List<GroupRepresentation> roleGroups = new ArrayList<>();
+        List<RoleRepresentation> roleRoles = new ArrayList<>();
+
+        UsersResource usersResource = KeycloakClient.getKeycloakClient().getRealm().users();
+        GroupsResource groupsResource = KeycloakClient.getKeycloakClient().getRealm().groups();
+        RolesResource rolesResource      = KeycloakClient.getKeycloakClient().getRealm().roles();
+
+        if(CollectionUtils.isNotEmpty(users)) {
+            roleUsers = users.stream().map(user -> {
+                List<UserRepresentation> matchedUsers = usersResource.search(user);
+                Optional<UserRepresentation> keyUserOptional = matchedUsers.stream().filter(x -> user.equals(x.getUsername())).findFirst();
+                if (keyUserOptional.isPresent()) {
+                    return keyUserOptional.get();
+                }
+                return null;
+            }).filter(Objects::nonNull).collect(Collectors.toList());
+        }
+
+        if(CollectionUtils.isNotEmpty(groups)) {
+            roleGroups = groups.stream().map(group -> {
+                List<GroupRepresentation> matchedGroups = groupsResource.groups(group, 0, 1);
+                Optional<GroupRepresentation> keyGroupOptional = matchedGroups.stream().filter(x -> group.equals(x.getName())).findFirst();
+                if (keyGroupOptional.isPresent()) {
+                    return keyGroupOptional.get();
+                }
+                return null;
+            }).filter(Objects::nonNull).collect(Collectors.toList());
+        }
+
+        if(CollectionUtils.isNotEmpty(roles)) {
+            roleRoles = roles.stream().map(role -> {
+                List<RoleRepresentation> matchedRoles = rolesResource.list();
+                Optional<RoleRepresentation> keyRoleOptional = matchedRoles.stream().filter(x -> role.equals(x.getId())).findFirst();
+                if (keyRoleOptional.isPresent()) {
+                    return keyRoleOptional.get();
+                }
+                return null;
+            }).filter(Objects::nonNull).collect(Collectors.toList());
+        }
+
+        RoleRepresentation role = new RoleRepresentation();
+        role.setName(roleName);
+        role.setComposite(true);
+
+        RoleRepresentation createdRole = keycloakStore.createRole(role);
+        if (createdRole == null) {
+            throw new AtlasBaseException(AtlasErrorCode.INTERNAL_ERROR, "Failed to create role " + roleName);
+        }
+        RoleResource compositeRoleResource = rolesResource.get(createdRole.getName());
+
+        //Add realm role to users
+        for (UserRepresentation user : roleUsers) {
+            UserResource userResource = usersResource.get(user.getId());
+            userResource.roles().realmLevel().add(Collections.singletonList(createdRole));
+            userResource.update(user);
+        }
+
+        //Add realm role to groups
+        for (GroupRepresentation group : roleGroups) {
+            GroupResource groupResource = groupsResource.group(group.getId());
+            groupResource.roles().realmLevel().add(Collections.singletonList(createdRole));
+            groupResource.update(group);
+        }
+
+        //Add realm role to roles
+        for (RoleRepresentation roleToAdd : roleRoles) {
+            compositeRoleResource.addComposites(Collections.singletonList(roleToAdd));
+            compositeRoleResource.update(createdRole);
+        }
+
     }
 }
