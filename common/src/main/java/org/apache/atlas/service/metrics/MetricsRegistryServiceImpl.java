0bf74fea5867f4e23ebf439bf24f48ddd2222816
naveenaechan <naveen.aechan@atlan.com>
2023-07-05 21:52:35 +0530

    PLT-1173: tune distribution

--- common/src/main/java/org/apache/atlas/service/metrics/MetricsRegistryServiceImpl.java
+++ common/src/main/java/org/apache/atlas/service/metrics/MetricsRegistryServiceImpl.java
@@ -7,7 +7,6 @@ import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import org.apache.atlas.annotation.ConditionalOnAtlasProperty;
 import org.apache.atlas.utils.AtlasPerfMetrics;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
@@ -24,14 +23,22 @@ public class MetricsRegistryServiceImpl implements MetricsRegistry {
     private static final String NAME = "name";
     private static final String METHOD_DIST_SUMMARY = "method_dist_summary";
     private static final double[] PERCENTILES = {0.90, 0.99};
-    private static final int SEC_MILLIS_SCALE = 1000;
+    private static final int SEC_MILLIS_SCALE = 1;
 
     private final PrometheusMeterRegistry prometheusMeterRegistry;
+    private final DistributionStatisticConfig distributionStatisticConfig;
 
     @Inject
-    public MetricsRegistryServiceImpl(PrometheusMeterRegistry prometheusMeterRegistry, ApplicationContext applicationContext) {
+    public MetricsRegistryServiceImpl(PrometheusMeterRegistry prometheusMeterRegistry) {
         this.prometheusMeterRegistry = prometheusMeterRegistry;
         this.prometheusMeterRegistry.config().withHighCardinalityTagsDetector().commonTags(SERVICE, ATLAS_METASTORE);
+        this.distributionStatisticConfig =  DistributionStatisticConfig.builder().percentilePrecision(2)
+                                            .percentiles(PERCENTILES)
+                                            .bufferLength(3)
+                                            .percentilesHistogram(false)
+                                            .minimumExpectedValue(1.0)
+                                            .maximumExpectedValue(Double.MAX_VALUE)
+                                            .expiry(Duration.ofMinutes(2)).build();
     }
 
     @Override
@@ -40,14 +47,8 @@ public class MetricsRegistryServiceImpl implements MetricsRegistry {
             AtlasPerfMetrics.Metric metric = metrics.getMetric(name);
             this.prometheusMeterRegistry.newDistributionSummary(new Meter.Id(METHOD_DIST_SUMMARY,
                                     Tags.of(NAME, metric.getName()), BaseUnits.MILLISECONDS, METHOD_DIST_SUMMARY, Meter.Type.TIMER),
-                                     DistributionStatisticConfig.builder().percentilePrecision(2)
-                                    .percentiles(PERCENTILES)
-                                    .bufferLength(3)
-                                    .percentilesHistogram(false)
-                                    .minimumExpectedValue(1.0)
-                                    .maximumExpectedValue(Double.MAX_VALUE)
-                                    .expiry(Duration.ofMinutes(1)).build(), SEC_MILLIS_SCALE)
-                                    .record(metric.getTotalTimeMSecs());
+                            distributionStatisticConfig, SEC_MILLIS_SCALE)
+                    .record(metric.getTotalTimeMSecs());
         }
     }
 
