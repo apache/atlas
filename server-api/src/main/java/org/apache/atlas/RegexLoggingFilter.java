9a542d170e81520288b57c05dda5bcec56ff0007
Nikhil P Bonte <nikhil.bonte@atlan.com>
2023-08-24 15:52:21 +0530

    GOV-1295 Custom Regex logging filter

--- server-api/src/main/java/org/apache/atlas/RegexLoggingFilter.java
+++ server-api/src/main/java/org/apache/atlas/RegexLoggingFilter.java
@@ -1,13 +1,12 @@
 package org.apache.atlas;
 
-import java.util.logging.LogRecord;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.log4j.spi.Filter;
 import org.apache.log4j.spi.LoggingEvent;
 
-public class LoggingFilterCustom extends Filter {
+public class RegexLoggingFilter extends Filter {
     private Pattern pattern;
 
     public void setPatternString(String patternString) {
