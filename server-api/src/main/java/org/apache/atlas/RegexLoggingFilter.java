499f7180ad01c693a58e6f555d697cf427ca665d
Nikhil P Bonte <nikhil.bonte@atlan.com>
2023-08-24 15:46:02 +0530

    GOV-1295 Custom Regex logging filter

--- server-api/src/main/java/org/apache/atlas/RegexLoggingFilter.java
+++ server-api/src/main/java/org/apache/atlas/RegexLoggingFilter.java
@@ -8,17 +8,18 @@ import org.apache.log4j.spi.Filter;
 import org.apache.log4j.spi.LoggingEvent;
 
 public class LoggingFilterCustom extends Filter {
-    private static final Pattern PATTERN;
+    private Pattern pattern;
 
-    static {
-        PATTERN = Pattern.compile("^slave: redis:.* is down");
+    public void setPatternString(String patternString) {
+        pattern = Pattern.compile(patternString);
     }
-//Pattern.compile("^slave: redis:.* is down").matcher("slave: redis://10.192.41.100:6379 is down").matches()
+
+    //Pattern.compile("^slave: redis:.* is down").matcher("slave: redis://10.192.41.100:6379 is down").matches()
     @Override
     public int decide(LoggingEvent event) {
         String message = (String) event.getMessage();
 
-        final Matcher m = PATTERN.matcher(message);
+        final Matcher m = pattern.matcher(message);
         return m.matches() ? DENY : NEUTRAL;
     }
 }
