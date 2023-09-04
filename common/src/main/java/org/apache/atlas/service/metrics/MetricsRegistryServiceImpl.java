2fd5d4a7917a02c4fa9e12eea2a570bcdd72e592
naveenaechan <naveen.aechan@atlan.com>
2023-09-04 20:42:37 +0530

    PLT-1879: added options to enable method level observability metrics

--- common/src/main/java/org/apache/atlas/service/metrics/MetricsRegistryServiceImpl.java
+++ common/src/main/java/org/apache/atlas/service/metrics/MetricsRegistryServiceImpl.java
@@ -46,15 +46,15 @@ public class MetricsRegistryServiceImpl implements MetricsRegistry {
             if (!ApplicationProperties.get().getBoolean(METHOD_LEVEL_METRICS_ENABLE, false)) {
                 return;
             }
+
+            for (String name : this.filteredMethods) {
+                AtlasPerfMetrics.Metric metric = metrics.getMetric(name);
+                Timer.builder(METHOD_DIST_SUMMARY).tags(Tags.of(NAME, metric.getName())).publishPercentiles(PERCENTILES)
+                        .register(getMeterRegistry()).record(metric.getTotalTimeMSecs(), TimeUnit.MILLISECONDS);
+            }
         } catch (AtlasException e) {
             LOG.error("Failed to read {} property from atlas config", METHOD_LEVEL_METRICS_ENABLE, e);
             return;
-        }
-
-        for (String name : this.filteredMethods) {
-            AtlasPerfMetrics.Metric metric = metrics.getMetric(name);
-            Timer.builder(METHOD_DIST_SUMMARY).tags(Tags.of(NAME, metric.getName())).publishPercentiles(PERCENTILES)
-                    .register(getMeterRegistry()).record(metric.getTotalTimeMSecs(), TimeUnit.MILLISECONDS);
         }
     }
 
