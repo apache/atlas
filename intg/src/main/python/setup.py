e86406906ebdf21056952f238478d2b5a9d48441
Madhan Neethiraj <madhan@apache.org>
2023-12-01 15:17:44 -0800

    ATLAS-4815: publish Python client library with the fix from previous commit

--- intg/src/main/python/setup.py
+++ intg/src/main/python/setup.py
@@ -28,7 +28,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='apache-atlas',
-    version='0.0.14',
+    version='0.0.15',
     author="Apache Atlas",
     author_email='dev@atlas.apache.org',
     description="Apache Atlas Python Client",
