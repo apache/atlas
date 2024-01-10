641fae7c3f8317af0839901d0da3906f76166abe
Suman Das <59254445+sumandas0@users.noreply.github.com>
2024-01-10 19:32:15 +0530

    nit: adding function refactoring

--- client-auth/src/main/java/org/apache/atlas/auth/client/heracles/AtlasHeraclesClient.java
+++ client-auth/src/main/java/org/apache/atlas/auth/client/heracles/AtlasHeraclesClient.java
@@ -44,7 +44,7 @@ public class AtlasHeraclesClient {
     public List<UserRepresentation> getUsersMappings(int start, int size) throws AtlasBaseException {
         String[] columns = {"roles", "groups"};
         List<HeraclesUserViewRepresentation> views =  HERACLES.getUsersMappings(start, size, HeraclesUserViewRepresentation.sortBy, columns).body();
-        List<UserRepresentation> userRepresentations = views.stream().map(x -> {
+        return views.stream().map(x -> {
             UserRepresentation userRepresentation = new UserRepresentation();
             userRepresentation.setId(x.getId());
             userRepresentation.setUsername(x.getUsername());
@@ -52,7 +52,6 @@ public class AtlasHeraclesClient {
             userRepresentation.setGroups(x.getGroups());
             return userRepresentation;
         }).collect(Collectors.toList());
-        return userRepresentations;
     }
 
     public List<HeraclesRoleViewRepresentation> getRolesMappings(int start, int size) throws AtlasBaseException {
