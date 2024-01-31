4ab94f7992f89c57aa53cafb23baa79a051bb341
Suman Das <59254445+sumandas0@users.noreply.github.com>
2024-01-31 12:30:48 +0530

    re: add spacing

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphRetriever.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphRetriever.java
@@ -1047,7 +1047,7 @@ public class EntityGraphRetriever {
         catch (NullPointerException npe) {
             String id = entityVertex.getIdForDisplay()==null ? "null" :entityVertex.getIdForDisplay();
             LOG.error("mapVertexToAtlasEntityHeader: failed for entityVertex with id {}", id, npe);
-            throw new AtlasBaseException(AtlasErrorCode.UNKNOWN_SERVER_ERROR, npe, "mapVertexToAtlasEntityHeader: failed for entityVertex with id" + id);
+            throw new AtlasBaseException(AtlasErrorCode.UNKNOWN_SERVER_ERROR, npe, "mapVertexToAtlasEntityHeader: failed for entityVertex with id " + id);
         }
         finally {
             RequestContext.get().endMetricRecord(metricRecorder);
