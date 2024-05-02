686a9e23a652963d28641e63b41cdc8581355749
Suman Das <59254445+sumandas0@users.noreply.github.com>
2024-05-03 00:01:07 +0530

    feat: add local search context cache as backup

--- graphdb/janus/src/main/java/org/apache/atlas/repository/graphdb/janus/SearchContextCache.java
+++ graphdb/janus/src/main/java/org/apache/atlas/repository/graphdb/janus/SearchContextCache.java
@@ -3,20 +3,21 @@ package org.apache.atlas.repository.graphdb.janus;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import org.apache.atlas.RequestContext;
-import org.apache.atlas.service.redis.AbstractRedisService;
 import org.apache.atlas.service.redis.RedisService;
 import org.apache.atlas.utils.AtlasPerfMetrics;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Component
 public class SearchContextCache {
     private static RedisService redisService = null;
+
+    private static final Cache<String, String> searchContextLocalCache = CacheBuilder.newBuilder()
+            .maximumSize(200)
+            .expireAfterWrite(30, TimeUnit.SECONDS)
+            .build();
 
     public static final String INVALID_SEQUENCE = "invalid_sequence";
 
@@ -32,12 +33,23 @@ public class SearchContextCache {
            // Build the string in format `sequence/esAsyncId` and store it in redis
            String val = sequence + "/" + esAsyncId;
            redisService.putValue(key, val);
+           searchContextLocalCache.put(key, val);
        } finally {
            RequestContext.get().endMetricRecord(metric);
        }
     }
-    public static String get(String key){
-        return redisService.getValue(key);
+    public static String get(String key) {
+        String ret = null;
+        try {
+            ret = searchContextLocalCache.getIfPresent(key);
+            if (ret == null) {
+                ret = redisService.getValue(key);
+            }
+            return ret;
+        } catch (Exception e) {
+            return null;
+        }
+
     }
 
     public static String getESAsyncSearchIdFromContextCache(String key, Integer sequence){
@@ -68,6 +80,7 @@ public class SearchContextCache {
     public static void remove(String key) {
         AtlasPerfMetrics.MetricRecorder metric = RequestContext.get().startMetricRecord("removeFromCache");
         try {
+            searchContextLocalCache.invalidate(key);
             redisService.removeValue(key);
         } finally {
             RequestContext.get().endMetricRecord(metric);
