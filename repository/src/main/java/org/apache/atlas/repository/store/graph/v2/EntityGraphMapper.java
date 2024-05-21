eaf8f5b50cfa3bce2ac84ca4fa8a68fc03166dc8
Suman Das <59254445+sumandas0@users.noreply.github.com>
2024-05-21 18:07:44 +0530

    fix: add Autowired to the import

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
@@ -86,6 +86,7 @@ import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
