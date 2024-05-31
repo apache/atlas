a21e0f7962e2730d9318b255e5a6a8da14416ea1
arpit-at <arpit.upadhyay@atlan.com>
2024-05-31 12:48:21 +0530

    DG-1374 Update in pre-processor to handle the adminlist

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/ConnectionPreProcessor.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/ConnectionPreProcessor.java
@@ -17,7 +17,6 @@
  */
 package org.apache.atlas.repository.store.graph.v2.preprocessor;
 
-import org.apache.atlas.AtlasErrorCode;
 import org.apache.atlas.DeleteType;
 import org.apache.atlas.RequestContext;
 import org.apache.atlas.discovery.EntityDiscoveryService;
@@ -172,13 +171,13 @@ public class ConnectionPreProcessor implements PreProcessor {
             entity.setAttribute(QUALIFIED_NAME, vertexQName);
 
             RoleRepresentation representation = getKeycloakClient().getRoleByName(roleName);
-           // String creatorUser = vertex.getProperty(CREATED_BY_KEY, String.class);
+            String creatorUser = vertex.getProperty(CREATED_BY_KEY, String.class);
 
             if (connection.hasAttribute(ATTR_ADMIN_USERS)) {
                 List<String> newAdminUsers = (List<String>) connection.getAttribute(ATTR_ADMIN_USERS);
                 List<String> currentAdminUsers = (List<String>) existingConnEntity.getAttribute(ATTR_ADMIN_USERS);
-                if (CollectionUtils.isEmpty(newAdminUsers)) {
-                    throw new AtlasBaseException(AtlasErrorCode.ADMIN_LIST_SHOULD_NOT_BE_EMPTY, connection.getTypeName());
+                if (StringUtils.isNotEmpty(creatorUser) && CollectionUtils.isEmpty(newAdminUsers)) {
+                    newAdminUsers.add(creatorUser);
                 }
 
                 connection.setAttribute(ATTR_ADMIN_USERS, newAdminUsers);
