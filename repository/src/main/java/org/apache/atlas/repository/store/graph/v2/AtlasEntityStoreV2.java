4a3c887981a13f9c96f19f43ff4b249d58687387
arpit-at <arpit.upadhyay@atlan.com>
2024-07-24 14:19:59 +0530

    DG-1697: PR comments resolved

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasEntityStoreV2.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasEntityStoreV2.java
@@ -2734,6 +2734,7 @@ public class AtlasEntityStoreV2 implements AtlasEntityStore {
             if (CollectionUtils.isEmpty(vertices)) {
                 return;
             }
+
             handleBusinessPolicyMutation(vertices);
         } catch (Exception e) {
             LOG.error("Error during linkBusinessPolicy for policyGuid: {}", policyGuid, e);
@@ -2754,6 +2755,7 @@ public class AtlasEntityStoreV2 implements AtlasEntityStore {
             if (CollectionUtils.isEmpty(vertices)) {
                 return;
             }
+
             handleBusinessPolicyMutation(vertices);
         } catch (Exception e) {
             LOG.error("Error during unlinkBusinessPolicy for policyGuid: {}", policyGuid, e);
