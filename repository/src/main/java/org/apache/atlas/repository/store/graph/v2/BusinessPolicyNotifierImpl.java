c9b0274f87ab8a4355602062fe6b7202434ba918
arpit-at <arpit.upadhyay@atlan.com>
2024-07-24 17:22:03 +0530

    DG-1697: PR comments resolved

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/BusinessPolicyNotifierImpl.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/BusinessPolicyNotifierImpl.java
@@ -34,7 +34,7 @@ public class BusinessPolicyNotifierImpl implements IAtlasAlternateChangeNotifier
                                    final Map<String, Map<String, Object>> attributesByGuid) throws AtlasBaseException {
         AtlasPerfMetrics.MetricRecorder metricRecorder = RequestContext.get().startMetricRecord("onEntitiesMutation");
         final List<AtlasEntity> entities = new ArrayList<>(0);
-        vertices.forEach(item -> entities.add(createAtlasEntity(item, attributesByGuid.get(item.getProperty(GUID_PROPERTY_KEY, String.class)))));
+        vertices.forEach(item -> entities.add(createAtlasEntity(item)));
         for (EntityChangeListenerV2 listener : entityChangeListenersV2) {
             listener.onEntitiesUpdated(entities, false);
         }
@@ -42,9 +42,13 @@ public class BusinessPolicyNotifierImpl implements IAtlasAlternateChangeNotifier
         RequestContext.get().endMetricRecord(metricRecorder);
     }
 
-    private AtlasEntity createAtlasEntity(AtlasVertex vertex, Map<String, Object> attributes) {
+    private AtlasEntity createAtlasEntity(AtlasVertex vertex) {
         AtlasEntity atlasEntity = new AtlasEntity();
+        Map<String, Object> attributes = new HashMap<>();
+        attributes.put(QUALIFIED_NAME, vertex.getProperty(QUALIFIED_NAME, String.class));
+        attributes.put(NAME, vertex.getProperty(NAME, String.class));
         atlasEntity.setAttributes(attributes);
+
         atlasEntity.setGuid(vertex.getProperty(GUID_PROPERTY_KEY, String.class));
         atlasEntity.setTypeName(vertex.getProperty(TYPE_NAME_PROPERTY_KEY, String.class));
         atlasEntity.setCreatedBy(vertex.getProperty(CREATED_BY_KEY, String.class));
@@ -58,6 +62,11 @@ public class BusinessPolicyNotifierImpl implements IAtlasAlternateChangeNotifier
         atlasEntity.setCustomAttributes(getCustomAttributes(vertex));
         atlasEntity.setHomeId(getHomeId(vertex));
         atlasEntity.setVersion(getVersion(vertex));
+
+        atlasEntity.setAttribute(NAME, vertex.getPropertyValues(NAME, String.class));
+        atlasEntity.setAttribute(EntityGraphRetriever.DESCRIPTION, vertex.getPropertyValues(EntityGraphRetriever.DESCRIPTION, String.class));
+        atlasEntity.setAttribute(OWNER_ATTRIBUTE, vertex.getPropertyValues(OWNER_ATTRIBUTE, String.class));
+        atlasEntity.setAttribute(EntityGraphRetriever.CREATE_TIME, new Date(vertex.getProperty(TIMESTAMP_PROPERTY_KEY, Long.class)));
 
         return atlasEntity;
     }
