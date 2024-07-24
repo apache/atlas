a5e7103bc5ba6f2d731908c6d24f0b1aeef34f23
arpit-at <arpit.upadhyay@atlan.com>
2024-07-24 18:47:12 +0530

    DG-1697: PR comments resolved

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/BusinessPolicyNotifierImpl.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/BusinessPolicyNotifierImpl.java
@@ -43,10 +43,8 @@ public class BusinessPolicyNotifierImpl implements IAtlasAlternateChangeNotifier
 
     private AtlasEntity createAtlasEntity(AtlasVertex vertex) {
         AtlasEntity atlasEntity = new AtlasEntity();
-        Map<String, Object> attributes = new HashMap<>();
-        attributes.put(QUALIFIED_NAME, vertex.getProperty(QUALIFIED_NAME, String.class));
-        attributes.put(NAME, vertex.getProperty(NAME, String.class));
-        atlasEntity.setAttributes(attributes);
+        atlasEntity.setAttribute(QUALIFIED_NAME, vertex.getProperty(QUALIFIED_NAME, String.class));
+        atlasEntity.setAttribute(NAME, vertex.getProperty(NAME, String.class));
 
         atlasEntity.setGuid(vertex.getProperty(GUID_PROPERTY_KEY, String.class));
         atlasEntity.setTypeName(vertex.getProperty(TYPE_NAME_PROPERTY_KEY, String.class));
@@ -62,9 +60,9 @@ public class BusinessPolicyNotifierImpl implements IAtlasAlternateChangeNotifier
         atlasEntity.setHomeId(getHomeId(vertex));
         atlasEntity.setVersion(getVersion(vertex));
 
-        atlasEntity.setAttribute(NAME, vertex.getPropertyValues(NAME, String.class));
-        atlasEntity.setAttribute(EntityGraphRetriever.DESCRIPTION, vertex.getPropertyValues(EntityGraphRetriever.DESCRIPTION, String.class));
-        atlasEntity.setAttribute(OWNER_ATTRIBUTE, vertex.getPropertyValues(OWNER_ATTRIBUTE, String.class));
+        atlasEntity.setAttribute(NAME, vertex.getProperty(NAME, String.class));
+        atlasEntity.setAttribute(EntityGraphRetriever.DESCRIPTION, vertex.getProperty(EntityGraphRetriever.DESCRIPTION, String.class));
+        atlasEntity.setAttribute(OWNER_ATTRIBUTE, vertex.getProperty(OWNER_ATTRIBUTE, String.class));
         atlasEntity.setAttribute(EntityGraphRetriever.CREATE_TIME, new Date(vertex.getProperty(TIMESTAMP_PROPERTY_KEY, Long.class)));
 
         return atlasEntity;
