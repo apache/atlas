83e7463fa87c3443423985e4506a248f6a28de0a
PRATHAM2002-DS <pratham.more@atlan.com>
2024-09-10 23:14:42 +0530

    mesh-192: handled null domain guid

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
@@ -4698,7 +4698,7 @@ public class EntityGraphMapper {
                 if (meshEntityId.isEmpty() || existingValues.contains(meshEntityId)) {
                     isAuthorizedToLink(ev);
 
-                    if (meshEntityId.isEmpty() || meshEntityId == null) {
+                    if (StringUtils.isEmpty(meshEntityId)) {
                         existingValues.clear();
                         ev.removeProperty(DOMAIN_GUIDS_ATTR);
                     } else {
