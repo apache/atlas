eacd456e5d756caf20fb79c8deb151acac33eb6f
arpit-at <arpit.upadhyay@atlan.com>
2024-09-11 14:16:14 +0530

    POLICY-139 API endpoint to move the policy

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasEntityStoreV2.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/AtlasEntityStoreV2.java
@@ -2870,6 +2870,10 @@ public class AtlasEntityStoreV2 implements AtlasEntityStore {
         this.atlasAlternateChangeNotifier.onEntitiesMutation(vertices);
         RequestContext.get().endMetricRecord(metricRecorder);
     }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 0724d62c5 (POLICY-139 API endpoint to move the policy)
     @Override
     @GraphTransaction
     public void moveBusinessPolicy(Set<String> policyIds, String assetId, String type) throws AtlasBaseException {
@@ -2896,6 +2900,9 @@ public class AtlasEntityStoreV2 implements AtlasEntityStore {
     }
 }
 
+<<<<<<< HEAD
 
 
 
+=======
+>>>>>>> 0724d62c5 (POLICY-139 API endpoint to move the policy)
