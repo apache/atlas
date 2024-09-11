8275720d024488b2ec43ba4564b3b68614e4c7f1
PRATHAM2002-DS <pratham.more@atlan.com>
2024-09-11 18:24:54 +0530

    mesh-184: access via tags error debug

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/AssetPreProcessor.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/AssetPreProcessor.java
@@ -114,7 +114,7 @@ public class AssetPreProcessor implements PreProcessor {
     }
 
     private void isAuthorized(AtlasVertex vertex) throws AtlasBaseException {
-        AtlasEntityHeader sourceEntity = retrieverNoRelation.toAtlasEntityHeader(vertex);
+        AtlasEntityHeader sourceEntity = entityRetriever.toAtlasEntityHeader(vertex);
 
         // source -> UPDATE + READ
         AtlasAuthorizationUtils.verifyAccess(new AtlasEntityAccessRequest(typeRegistry, AtlasPrivilege.ENTITY_UPDATE, sourceEntity),
