9e1c9e3bdc6f66a0f63f88d4caffcd793949c89d
sriram-atlan <sriram.aravamuthan@atlan.com>
2024-12-03 19:36:58 +0530

    print stacktrace

--- webapp/src/main/java/org/apache/atlas/web/errors/ExceptionMapperUtil.java
+++ webapp/src/main/java/org/apache/atlas/web/errors/ExceptionMapperUtil.java
@@ -34,7 +34,11 @@ public class ExceptionMapperUtil {
 
     @SuppressWarnings("UnusedParameters")
     protected static String formatLogMessage(long id, Throwable exception) {
-        return String.format("Error handling a request: %016x , exception is: %s", id, exception.getMessage());
+        StringBuilder stackTrace = new StringBuilder();
+        for (StackTraceElement element : exception.getStackTrace()) {
+            stackTrace.append(element.toString()).append("\n");
+        }
+        return String.format("Error handling a request: %016x , exception is: %s\nStack trace:\n%s", id, exception.getMessage(), stackTrace.toString());
     }
 
 }
