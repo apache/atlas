0111fbd83068c45a796a79033de1c2e04773193d
aarshi <aarshi.arora@atlan.com>
2025-01-28 17:38:23 +0530

    remove trace id

--- auth-audits/src/main/java/org/apache/atlas/audit/destination/Log4JAuditDestination.java
+++ auth-audits/src/main/java/org/apache/atlas/audit/destination/Log4JAuditDestination.java
@@ -46,7 +46,6 @@ public class Log4JAuditDestination extends AuditDestination {
 	private static final String AUTH_AUDIT_RESOURCE= "resource";
 	private static final String AUTH_AUDIT_CLIENT_IP = "cliIP";
 	private static final String AUTH_AUDIT_AGENT = "agent";
-	private static final String TRACE_ID = "trace_id";
 
 
 	public Log4JAuditDestination() {
@@ -105,7 +104,6 @@ public class Log4JAuditDestination extends AuditDestination {
             MDC.put(AUTH_AUDIT_RESULT, String.valueOf(((AuthzAuditEvent) event).getAccessResult()));
 			MDC.put(AUTH_AUDIT_CLIENT_IP, ((AuthzAuditEvent) event).getClientIP());
 			MDC.put(AUTH_AUDIT_AGENT, ((AuthzAuditEvent) event).getAgentId());
-			MDC.put(TRACE_ID, MDC.get(TRACE_ID));
         }
     }
 
