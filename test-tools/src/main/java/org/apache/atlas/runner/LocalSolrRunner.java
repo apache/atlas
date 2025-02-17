7c5d18c2a5a2ee05b7daf653d44cce225cc5869a
Jayendra Parab <jayendra@freestoneinfotech.com>
2025-02-17 22:05:40 +0530

    ATLAS-4967: atlas-testtools module: update for code readability improvements

--- test-tools/src/main/java/org/apache/atlas/runner/LocalSolrRunner.java
+++ test-tools/src/main/java/org/apache/atlas/runner/LocalSolrRunner.java
@@ -1,20 +1,17 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2017 JanusGraph Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package org.apache.atlas.runner;
 
 import org.apache.atlas.ApplicationProperties;
@@ -44,14 +41,12 @@ public class LocalSolrRunner {
     private static final Logger LOG = LoggerFactory.getLogger(LocalSolrRunner.class);
 
     // from org.apache.atlas.repository.graphdb.janus.AtlasJanusGraphDatabase
-    public static final String SOLR_ZOOKEEPER_URL = "atlas.graph.index.search.solr.zookeeper-url";
-
-    protected static final String[] COLLECTIONS = readCollections();
-
-    private static final String TARGET_DIRECTORY   = System.getProperty("embedded.solr.directory");
-    private static final String COLLECTIONS_FILE   = "collections.txt";
-    private static final String SOLR_XML           = "solr.xml";
-    private static final String TEMPLATE_DIRECTORY = "core-template";
+    public  static final String SOLR_ZOOKEEPER_URL = "atlas.graph.index.search.solr.zookeeper-url";
+    protected static final String[] COLLECTIONS        = readCollections();
+    private static final   String   TARGET_DIRECTORY   = System.getProperty("embedded.solr.directory");
+    private static final   String   COLLECTIONS_FILE   = "collections.txt";
+    private static final   String   SOLR_XML           = "solr.xml";
+    private static final   String   TEMPLATE_DIRECTORY = "core-template";
 
     private static MiniSolrCloudCluster miniSolrCloudCluster;
 
@@ -72,7 +67,7 @@ public class LocalSolrRunner {
 
         miniSolrCloudCluster = new MiniSolrCloudCluster(1, null, temp.toPath(), readSolrXml(), null, null);
 
-        LOG.info("Started local solr server at: {}", getZookeeperUrls());
+        LOG.info("Started local solr server at: " + getZookeeperUrls());
 
         for (String coreName : COLLECTIONS) {
             File coreDirectory = new File(temp.getAbsolutePath() + File.separator + coreName);
@@ -104,7 +99,6 @@ public class LocalSolrRunner {
         if (!isLocalSolrRunning()) {
             return;
         }
-
         System.clearProperty("solr.solrxml.location");
         System.clearProperty("zkHost");
 
@@ -151,12 +145,10 @@ public class LocalSolrRunner {
         // For the classloader you need the following path: "/solr/collections.txt";
         // Use explicit '/' separators (not File.separator) because even on Windows you want '/'
         String resName = "/solr/" + COLLECTIONS_FILE;
-
         try {
             InputStream       inputStream = LocalSolrRunner.class.getResourceAsStream(resName);
             InputStreamReader isr         = new InputStreamReader(inputStream);
             BufferedReader    buffer      = new BufferedReader(isr);
-
             return Pattern.compile("\\s+").split(buffer.lines().collect(Collectors.joining("\n")));
         } catch (Exception e) {
             throw new RuntimeException("Unable to read collections file", e);
@@ -167,7 +159,6 @@ public class LocalSolrRunner {
         // For the classloader you need the following path: "/solr/solr.xml";
         // Use explicit '/' separators (not File.separator) because even on Windows you want '/'
         String resName = "/solr/" + SOLR_XML;
-
         // Use the local classloader rather than the system classloader - i.e. avoid using
         // Thread.currentThread().getContextClassLoader().getResourceAsStream(resName);
         InputStream inputStream = LocalSolrRunner.class.getResourceAsStream(resName);
