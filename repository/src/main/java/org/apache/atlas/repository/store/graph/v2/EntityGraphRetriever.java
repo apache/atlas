78c5239e665fc01f1603435ef7bbb9b7cd694858
sriram-atlan <sriram.aravamuthan@atlan.com>
2025-03-13 14:22:29 +0530

    MLH-207 classifications optimisation

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphRetriever.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphRetriever.java
@@ -50,7 +50,9 @@ import org.apache.atlas.repository.graphdb.AtlasElement;
 import org.apache.atlas.repository.graphdb.AtlasGraph;
 import org.apache.atlas.repository.graphdb.AtlasVertex;
 import org.apache.atlas.repository.graphdb.janus.AtlasJanusEdge;
+import org.apache.atlas.repository.graphdb.janus.AtlasJanusGraph;
 import org.apache.atlas.repository.graphdb.janus.AtlasJanusVertex;
+import org.apache.atlas.repository.graphdb.janus.GraphDbObjectFactory;
 import org.apache.atlas.repository.util.AccessControlUtils;
 import org.apache.atlas.type.AtlasArrayType;
 import org.apache.atlas.type.AtlasBuiltInTypes.AtlasObjectIdType;
@@ -70,6 +72,7 @@ import org.apache.atlas.v1.model.instance.Id;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.tinkerpop.gremlin.structure.T;
 import org.apache.tinkerpop.gremlin.structure.VertexProperty;
 import org.janusgraph.core.Cardinality;
 import org.janusgraph.graphdb.relations.CacheVertexProperty;
@@ -86,6 +89,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static org.apache.atlas.glossary.GlossaryUtils.TERM_ASSIGNMENT_ATTR_CONFIDENCE;
 import static org.apache.atlas.glossary.GlossaryUtils.TERM_ASSIGNMENT_ATTR_CREATED_BY;
@@ -1437,28 +1441,40 @@ public class EntityGraphRetriever {
     public List<AtlasClassification> getAllClassifications(AtlasVertex entityVertex) throws AtlasBaseException {
         AtlasPerfMetrics.MetricRecorder metricRecorder = RequestContext.get().startMetricRecord("getAllClassifications");
 
-        if(LOG.isDebugEnabled()){
-            LOG.debug("Performing getAllClassifications");
-        }
-        List<AtlasClassification> ret   = new ArrayList<>();
-        Iterable                  edges = entityVertex.query().direction(AtlasEdgeDirection.OUT).label(CLASSIFICATION_LABEL).edges();
-
-        if (edges != null) {
-            Iterator<AtlasEdge> iterator = edges.iterator();
-
-            while (iterator.hasNext()) {
-                AtlasEdge           classificationEdge   = iterator.next();
-                AtlasVertex         classificationVertex = classificationEdge != null ? classificationEdge.getInVertex() : null;
-                AtlasClassification classification       = toAtlasClassification(classificationVertex);
-
-                if (classification != null) {
-                    ret.add(classification);
-                }
+        try {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Performing getAllClassifications");
             }
-        }
 
-        RequestContext.get().endMetricRecord(metricRecorder);
-        return ret;
+            // Fetch classification vertices directly
+            List<AtlasVertex> classificationVertices = ((AtlasJanusGraph) graph).getGraph().traversal()
+                    .V(entityVertex.getId())  // Start from the entity vertex
+                    .outE(CLASSIFICATION_LABEL) // Get outgoing classification edges
+                    .inV() // Move to classification vertex
+                    .dedup() // Remove duplicate classification vertices
+                    .toList() // Convert to List<Vertex>
+                    .stream()
+                    .map(m -> GraphDbObjectFactory.createVertex(((AtlasJanusGraph) graph), m)) // Convert Vertex to AtlasVertex
+                    .collect(Collectors.toList());
+
+            return classificationVertices.stream()
+                    .map(m -> {
+                        try {
+                            return toAtlasClassification(m);
+                        } catch (AtlasBaseException e) {
+                            LOG.error("Error while getting all classifications", e);
+                            return null;
+                        }
+                    }) // Convert to AtlasClassification
+                    .filter(Objects::nonNull) // Remove null classifications
+                    .collect(Collectors.toList()); // Collect as a list
+
+        } catch (Exception e) {
+            LOG.error("Error while getting all classifications", e);
+            throw new AtlasBaseException(AtlasErrorCode.INTERNAL_ERROR, e);
+        } finally {
+            RequestContext.get().endMetricRecord(metricRecorder);
+        }
     }
 
     public List<AtlasTermAssignmentHeader> mapAssignedTerms(AtlasVertex entityVertex) {
