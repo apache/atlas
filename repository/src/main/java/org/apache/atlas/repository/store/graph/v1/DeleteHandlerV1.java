08f1c00012d39e328bd2dac860fd5689e5901445
dhanyavg-atlan <dhanya.v@atlan.com>
2025-03-25 10:45:24 +0530

    Handle null edges properly

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v1/DeleteHandlerV1.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v1/DeleteHandlerV1.java
@@ -1382,12 +1382,17 @@ public abstract class DeleteHandlerV1 {
             return;
         }
 
+        if (edge == null) {
+            LOG.warn("Edge is null, can't schedule task now");
+            return;
+        }
+
         String      currentUser         = RequestContext.getCurrentUser();
         boolean     isRelationshipEdge  = isRelationshipEdge(edge);
         boolean     isTermEntityEdge    = GraphHelper.isTermEntityEdge(edge);
 
-        if (edge == null || !isRelationshipEdge) {
-            LOG.warn("Edge is null or it is not relationship edge, can't schedule task now");
+        if (!isRelationshipEdge) {
+            LOG.warn("Edge is not relationship edge, can't schedule task now");
             return;
         }
 
