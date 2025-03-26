1090671e1004ec47f1fdff1f8afb7fe163b1ce9d
Pratham More <79985042+PRATHAM2002-DS@users.noreply.github.com>
2025-03-26 23:25:26 +0530

    Revert "MESH-464 : Changed event structure for outputPorts"

--- intg/src/main/java/org/apache/atlas/model/notification/EntityNotification.java
+++ intg/src/main/java/org/apache/atlas/model/notification/EntityNotification.java
@@ -111,7 +111,6 @@ public class EntityNotification implements Serializable {
         private OperationType           operationType;
         private long              eventTime;
         private Object       mutatedDetails;
-        private Object       mutatedRelationshipDetails;
         private Map<String, String> headers;
 
         public EntityNotificationV2() {
@@ -145,18 +144,6 @@ public class EntityNotification implements Serializable {
             setHeaders(requestContextHeaders);
         }
 
-        public EntityNotificationV2(AtlasEntityHeader entity, Object mutatedDetails, Object mutatedRelationshipDetails, OperationType operationType,
-                                    long eventTime, Map<String, String> requestContextHeaders) {
-            super(ENTITY_NOTIFICATION_V2);
-
-            setEntity(entity);
-            setOperationType(operationType);
-            setEventTime(eventTime);
-            setMutatedDetails(mutatedDetails);
-            setMutatedRelationshipDetails(mutatedRelationshipDetails);
-            setHeaders(requestContextHeaders);
-        }
-
         public EntityNotificationV2(AtlasRelationshipHeader relationship, OperationType operationType,
                                     long eventTime, Map<String, String> requestContextHeaders) {
             super(ENTITY_NOTIFICATION_V2);
@@ -181,14 +168,6 @@ public class EntityNotification implements Serializable {
 
         public Object getMutatedDetails(){
             return mutatedDetails;
-        }
-
-        public void setMutatedRelationshipDetails(Object mutatedRelationshipDetails){
-            this.mutatedRelationshipDetails = mutatedRelationshipDetails;
-        }
-
-        public Object getMutatedRelationshipDetails(){
-            return mutatedRelationshipDetails;
         }
 
 
