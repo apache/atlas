f7b9ca731c0f59137d011c9f6576f9c0d4b23a04
aarshi <aarshi.arora@atlan.com>
2025-04-01 17:53:10 +0530

    remove graphhelper

--- webapp/src/main/java/org/apache/atlas/web/rest/DiscoveryREST.java
+++ webapp/src/main/java/org/apache/atlas/web/rest/DiscoveryREST.java
@@ -90,7 +90,6 @@ public class DiscoveryREST {
     private final int                maxDslQueryLength;
     private final boolean            enableSearchLogging;
 
-    private final GraphHelper graphHelper;
     private final AtlasTypeRegistry     typeRegistry;
     private final AtlasDiscoveryService discoveryService;
     private final SearchLoggingManagement loggerManagement;
@@ -101,14 +100,13 @@ public class DiscoveryREST {
 
     @Inject
     public DiscoveryREST(AtlasTypeRegistry typeRegistry, AtlasDiscoveryService discoveryService,
-                         SearchLoggingManagement loggerManagement, Configuration configuration, GraphHelper graphHelper) {
+                         SearchLoggingManagement loggerManagement, Configuration configuration) {
         this.typeRegistry           = typeRegistry;
         this.discoveryService       = discoveryService;
         this.loggerManagement       = loggerManagement;
         this.maxFullTextQueryLength = configuration.getInt(Constants.MAX_FULLTEXT_QUERY_STR_LENGTH, 4096);
         this.maxDslQueryLength      = configuration.getInt(Constants.MAX_DSL_QUERY_STR_LENGTH, 4096);
         this.enableSearchLogging    = AtlasConfiguration.ENABLE_SEARCH_LOGGER.getBoolean();
-        this.graphHelper           = graphHelper;
     }
 
     /**
