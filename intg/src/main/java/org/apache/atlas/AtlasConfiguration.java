fcbac3f23b91d550c92ac50c434624baa7a14589
chaitalithombare <chaitalithombare@apache.org>
2025-04-22 19:00:47 +0530

    ATLAS-5017: Patch to replace the long strings set in spark_process attributes

--- intg/src/main/java/org/apache/atlas/AtlasConfiguration.java
+++ intg/src/main/java/org/apache/atlas/AtlasConfiguration.java
@@ -111,7 +111,8 @@ public enum AtlasConfiguration {
     ATLAS_AUDIT_DEFAULT_AGEOUT_IGNORE_TTL("atlas.audit.default.ageout.ignore.ttl", false),
     ATLAS_AUDIT_AGING_TTL_TEST_AUTOMATION("atlas.audit.aging.ttl.test.automation", false), //Only for test automation
     RELATIONSHIP_SEARCH_ENABLED("atlas.relationship.search.enabled", false),
-    UI_TASKS_TAB_USE_ENABLED("atlas.tasks.ui.tab.enabled", false);
+    UI_TASKS_TAB_USE_ENABLED("atlas.tasks.ui.tab.enabled", false),
+    REPLACE_HUGE_SPARK_PROCESS_ATTRIBUTES_PATCH("atlas.process.spark.attributes.update.patch", false);
 
     private static final Configuration APPLICATION_PROPERTIES;
 
