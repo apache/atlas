45d4d3d929349b51d5026810e3876b27740a13fc
PRATHAM2002-DS <pratham.more@atlan.com>
2025-04-29 12:35:16 +0530

    mesh-508: Block hard deleting Product if assets linked

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/datamesh/DataProductPreProcessor.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/datamesh/DataProductPreProcessor.java
@@ -5,6 +5,7 @@ import org.apache.atlas.DeleteType;
 import org.apache.atlas.RequestContext;
 import org.apache.atlas.exception.AtlasBaseException;
 import org.apache.atlas.model.instance.*;
+import org.apache.atlas.repository.graph.GraphHelper;
 import org.apache.atlas.repository.graphdb.AtlasEdge;
 import org.apache.atlas.repository.graphdb.AtlasEdgeDirection;
 import org.apache.atlas.repository.graphdb.AtlasGraph;
@@ -473,6 +474,10 @@ public class DataProductPreProcessor extends AbstractDomainPreProcessor {
                     if(!exp.getAtlasErrorCode().equals(AtlasErrorCode.INSTANCE_BY_UNIQUE_ATTRIBUTE_NOT_FOUND)){
                         throw exp;
                     }
+                }
+
+                if (hasLinkedAssets(productGuid, PRODUCT_GUIDS)) {
+                    throw new AtlasBaseException(AtlasErrorCode.OPERATION_NOT_SUPPORTED, "Product cannot be hard deleted because some assets are linked to this product");
                 }
             }
             if(RequestContext.get().getDeleteType() == DeleteType.SOFT || RequestContext.get().getDeleteType() == DeleteType.DEFAULT){
