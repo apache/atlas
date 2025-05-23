db21eacee4611675beb951ff457b1182affe222d
ankitpatnaik-atlan <ankit.patnaik@atlan.com>
2025-05-23 10:27:50 +0530

    mesh-522: resolved reviews

--- repository/src/main/java/org/apache/atlas/repository/store/aliasstore/ESAliasStore.java
+++ repository/src/main/java/org/apache/atlas/repository/store/aliasstore/ESAliasStore.java
@@ -253,10 +253,13 @@ public class ESAliasStore implements IndexAliasStore {
                             // Add all parent domains in the hierarchy
                             String currentPath = asset;
                             while (currentPath.contains("/domain/")) {
-                                allowClauseList.add(mapOf("term", mapOf(QUALIFIED_NAME, currentPath)));
                                 int lastDomainIndex = currentPath.lastIndexOf("/domain/");
                                 if (lastDomainIndex != -1) {
                                     currentPath = currentPath.substring(0, lastDomainIndex);
+                                    if (currentPath.endsWith("default")) {
+                                        continue;
+                                    }
+                                    terms.add(currentPath);
                                 }
                             }
                         } else {
