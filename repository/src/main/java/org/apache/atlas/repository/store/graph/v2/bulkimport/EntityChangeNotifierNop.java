5f4a23df2a877c6fd402b31aa552f6c6e207888f
hr2904 <hritik.rai@atlan.com>
2025-06-23 23:56:38 +0530

    HR : Fixed request context not being shared to notification handler thus missing users and missing headers on audits

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/bulkimport/EntityChangeNotifierNop.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/bulkimport/EntityChangeNotifierNop.java
@@ -17,6 +17,7 @@
  */
 package org.apache.atlas.repository.store.graph.v2.bulkimport;
 
+import org.apache.atlas.RequestContext;
 import org.apache.atlas.exception.AtlasBaseException;
 import org.apache.atlas.model.glossary.AtlasGlossaryTerm;
 import org.apache.atlas.model.instance.AtlasClassification;
@@ -53,12 +54,12 @@ public class EntityChangeNotifierNop implements IAtlasEntityChangeNotifier {
     }
 
     @Override
-    public void onClassificationPropagationAddedToEntities(List<AtlasEntity> entities, List<AtlasClassification> addedClassifications, boolean forceInline) throws AtlasBaseException {
+    public void onClassificationPropagationAddedToEntities(List<AtlasEntity> entities, List<AtlasClassification> addedClassifications, boolean forceInline, RequestContext requestContext) throws AtlasBaseException {
 
     }
 
     @Override
-    public void onClassificationUpdatedToEntitiesV2(List<AtlasEntity> entities, AtlasClassification updatedClassification, boolean forceInline) throws AtlasBaseException {
+    public void onClassificationUpdatedToEntitiesV2(List<AtlasEntity> entities, AtlasClassification updatedClassification, boolean forceInline, RequestContext requestContext) throws AtlasBaseException {
 
     }
 
@@ -78,7 +79,7 @@ public class EntityChangeNotifierNop implements IAtlasEntityChangeNotifier {
     }
 
     @Override
-    public void onClassificationPropagationDeleted(List<AtlasEntity> entities, AtlasClassification deletedClassification, boolean forceInline) throws AtlasBaseException {
+    public void onClassificationPropagationDeleted(List<AtlasEntity> entities, AtlasClassification deletedClassification, boolean forceInline, RequestContext requestContext) throws AtlasBaseException {
 
     }
 
--- server-api/src/main/java/org/apache/atlas/RequestContext.java
+++ server-api/src/main/java/org/apache/atlas/RequestContext.java
@@ -676,6 +676,12 @@ public class RequestContext {
         }
     }
 
+    public void setRequestContextHeaders(Map<String, String> requestContextHeaders) {
+        if (requestContextHeaders != null) {
+            this.requestContextHeaders.putAll(requestContextHeaders);
+        }
+    }
+
     public Map<String, String> getRequestContextHeaders() {
         return requestContextHeaders;
     }
