9a20708634dbc0281a5b4201a524e490b0e1e6ed
ankitpatnaik-atlan <ankit.patnaik@atlan.com>
2025-07-30 18:05:07 +0530

    mesh-580: breaking QN in chunks and manually setting it

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/datamesh/AbstractDomainPreProcessor.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/preprocessor/datamesh/AbstractDomainPreProcessor.java
@@ -192,12 +192,20 @@ public abstract class AbstractDomainPreProcessor implements PreProcessor {
                         entityType = typeRegistry.getEntityTypeByName(STAKEHOLDER_ENTITY_TYPE);
 
                         String currentDomainQualifiedName = (String) asset.getAttribute(ATTR_DOMAIN_QUALIFIED_NAME);
-                        String currentQualifiedName = (String) asset.getAttribute(QUALIFIED_NAME);
 
                         entity.setAttribute(ATTR_DOMAIN_QUALIFIED_NAME, updatedDomainQualifiedNames.get(currentDomainQualifiedName));
-                        entity.setAttribute(QUALIFIED_NAME, updatedDomainQualifiedNames.get(currentQualifiedName));
                         updatedAttributes.put(ATTR_DOMAIN_QUALIFIED_NAME, updatedDomainQualifiedNames.get(currentDomainQualifiedName));
-                        updatedAttributes.put(QUALIFIED_NAME, updatedDomainQualifiedNames.get(currentQualifiedName));
+
+                        String currentStakeholderQualifiedName = (String) asset.getAttribute(QUALIFIED_NAME);
+                        if (currentStakeholderQualifiedName != null) {
+                            String[] parts = currentStakeholderQualifiedName.split("/", 3);
+                            if (parts.length == 3 && "default".equals(parts[0])) {
+                                String uuid = parts[1];
+                                String newQualifiedName = String.format("default/%s/%s", uuid, updatedDomainQualifiedNames.get(currentDomainQualifiedName));
+                                entity.setAttribute(QUALIFIED_NAME, newQualifiedName);
+                                updatedAttributes.put(QUALIFIED_NAME, newQualifiedName);
+                            }
+                        }
                     } else if (entity.getTypeName().equals(STAKEHOLDER_TITLE_ENTITY_TYPE)) {
                         entityType = typeRegistry.getEntityTypeByName(STAKEHOLDER_TITLE_ENTITY_TYPE);
 
