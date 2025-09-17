bab5f6fc0f38f3334241f3da245fd4cc39b8b191
Suraj <suraj.gupta@atlan.com>
2025-09-17 15:29:20 +0530

    Removed redundant vars

--- repository/src/main/java/org/apache/atlas/tasks/TaskQueueWatcher.java
+++ repository/src/main/java/org/apache/atlas/tasks/TaskQueueWatcher.java
@@ -35,7 +35,6 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Collectors;
 
 public class TaskQueueWatcher implements Runnable {
     private static final Logger LOG = LoggerFactory.getLogger(TaskQueueWatcher.class);
