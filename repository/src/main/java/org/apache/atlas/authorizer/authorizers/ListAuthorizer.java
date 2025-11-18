398573c5829660dd9a47d9c77bf8ac4d3692396c
Nikhil Soni <nikhil.soni@atlan.com>
2025-11-18 12:13:09 +0530

    Use prefix instead of wildcard where possible

--- repository/src/main/java/org/apache/atlas/authorizer/authorizers/ListAuthorizer.java
+++ repository/src/main/java/org/apache/atlas/authorizer/authorizers/ListAuthorizer.java
@@ -154,7 +154,10 @@ public class ListAuthorizer {
         List<String> termsQualifiedNames = new ArrayList<>();
         for (String entity: entities) {
             if (!entity.equals("*")) {
-                if (entity.contains("*") || entity.contains("?")) {
+                if (entity.endsWith("*")) {
+                    String prefix = entity.substring(0, entity.length() - 1);
+                    shouldClauses.add(getMap("prefix", getMap("qualifiedName", prefix)));
+                } else if (entity.contains("*") || entity.contains("?")) {
                     shouldClauses.add(getMap("wildcard", getMap("qualifiedName", entity)));
                 } else {
                     termsQualifiedNames.add(entity);
