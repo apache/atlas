07e8eb9daa7614e77819087e07487466fbf02117
Prasad Pawar <prasad.pawar@freestoneinfotech.com>
2025-11-20 16:51:01 +0530

    Atlas UI -Security related issue

--- dashboardv3/public/js/views/site/Header.js
+++ dashboardv3/public/js/views/site/Header.js
@@ -131,8 +131,10 @@ define(['require',
         onRender: function() {
             var that = this;
             if (Globals.userLogedIn.status) {
-                that.$('.userName').html(Globals.userLogedIn.response.userName);
-            }
+                // Security fix: Use .text() instead of .html() to prevent XSS attacks
+               // .text() automatically escapes HTML and prevents script injection
+               that.$('.userName').text(Globals.userLogedIn.response.userName);
+           }
             if (this.options.fromDefaultSearch !== true) {
                 this.renderGlobalSearch();
             }
