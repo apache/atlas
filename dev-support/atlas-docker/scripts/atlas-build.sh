5ebde03e808a782fa15c88dfce2206f85ce2e76d
Pinal Shah <pinal.shah@cloudera.com>
2026-02-25 22:28:08 +0530

    suppress progress logs

--- dev-support/atlas-docker/scripts/atlas-build.sh
+++ dev-support/atlas-docker/scripts/atlas-build.sh
@@ -86,7 +86,7 @@ else
   done
 fi
 
-mvn ${ARG_PROFILES} ${ARG_SKIPTESTS} -DskipDocs clean verify
+mvn -T 8 ${ARG_PROFILES} ${ARG_SKIPTESTS} -DskipDocs clean verify --no-transfer-progress -B -V
 
 status=$?
 
