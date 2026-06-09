c086e29e3e479da65bdcd2d4a9d3ce030b5f6c84
Prasad Pawar <prasad.pawar@freestoneinfotech.com>
2026-06-09 14:54:41 +0530

    ATLAS-5296: ATLAS UI: React UI fixes, audit table pagination text, Masonry grid
    Fix RAT license headers on ATLAS-5296 PR files.
    
    Move ASF header before @ts-nocheck directives and repair
    corrupted license lines so apache-rat-plugin:check passes.

--- dashboard/src/views/BusinessMetadata/BusinessMetadataAtrributeForm.tsx
+++ dashboard/src/views/BusinessMetadata/BusinessMetadataAtrributeForm.tsx
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -14,8 +12,10 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.useForm
+ * limitations under the License.
  */
+
+// @ts-nocheck
 
 import { CustomButton, LightTooltip } from "@components/muiComponents";
 import {
