09b1b2f7e907801eb2d3012fa343df6cd97944b9
ramk <ramk@cloudera.com>
2026-07-07 08:37:12 +0530

    Bind embedded KRaft broker to configured bootstrap.servers for ITs.
    Derive EXTERNAL and CONTROLLER listeners from atlas.kafka.bootstrap.servers so Jetty and Failsafe JVMs share the same broker port, fixing NotificationHookConsumerIT topic timeouts on CI.

--- notification/src/main/java/org/apache/atlas/kafka/EmbeddedKafkaServer.java
+++ notification/src/main/java/org/apache/atlas/kafka/EmbeddedKafkaServer.java
@@ -22,8 +22,10 @@ import kafka.testkit.TestKitNodes;
 import org.apache.atlas.ApplicationProperties;
 import org.apache.atlas.AtlasException;
 import org.apache.atlas.service.Service;
+import org.apache.atlas.util.CommandHandlerUtility;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.ConfigurationConverter;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.utils.Exit;
@@ -47,7 +49,8 @@ public class EmbeddedKafkaServer implements Service {
     public static final String PROPERTY_PREFIX   = "atlas.kafka";
     public static final String PROPERTY_EMBEDDED = "atlas.notification.embedded";
 
-    private static final String ATLAS_KAFKA_DATA = "data";
+    private static final String ATLAS_KAFKA_DATA          = "data";
+    private static final int    MAX_RETRY_TO_ACQUIRE_PORT = 3;
 
     private final boolean       isEmbedded;
     private final Configuration applicationProperties;
@@ -85,6 +88,97 @@ public class EmbeddedKafkaServer implements Service {
     public void stop() {
         LOG.info("==> EmbeddedKafkaServer.stop(isEmbedded={})", isEmbedded);
 
+        shutdownClusterQuietly();
+
+        LOG.info("<== EmbeddedKafka.stop(isEmbedded={})", isEmbedded);
+    }
+
+    private void startKraftBroker() throws Exception {
+        overrideExitMethods();
+
+        File   logDir                = constructDir("kafka");
+        String configuredBootstrap   = properties.getProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG);
+
+        LOG.info("Starting embedded KRaft kafka (log.dir={}, bootstrap.servers={})", logDir.getAbsolutePath(), configuredBootstrap);
+
+        for (int attempt = 0; attempt < MAX_RETRY_TO_ACQUIRE_PORT; attempt++) {
+            try {
+                startKraftBrokerOnce(logDir, configuredBootstrap);
+                return;
+            } catch (Exception e) {
+                LOG.warn("Attempt {}: failed to start embedded KRaft kafka", attempt, e);
+
+                shutdownClusterQuietly();
+
+                if (attempt == MAX_RETRY_TO_ACQUIRE_PORT - 1) {
+                    throw e;
+                }
+
+                int port = parsePort(configuredBootstrap);
+
+                if (port > 0) {
+                    CommandHandlerUtility.tryKillingProcessUsingPort(port, attempt != 0);
+                }
+            }
+        }
+    }
+
+    private void startKraftBrokerOnce(File logDir, String configuredBootstrap) throws Exception {
+        KafkaClusterTestKit.Builder clusterBuilder = new KafkaClusterTestKit.Builder(
+                new TestKitNodes.Builder()
+                        .setCombined(true)
+                        .setNumBrokerNodes(1)
+                        .setNumControllerNodes(1)
+                        .build());
+
+        Properties brokerConfig = buildBrokerConfig(logDir, configuredBootstrap);
+
+        brokerConfig.forEach((key, value) -> clusterBuilder.setConfigProp(key.toString(), value));
+
+        cluster = clusterBuilder.build();
+        cluster.format();
+        cluster.startup();
+        cluster.waitForReadyBrokers();
+
+        String bootstrapServers = StringUtils.isNotEmpty(configuredBootstrap)
+                ? configuredBootstrap
+                : cluster.clientProperties().get(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG).toString();
+
+        LOG.info("Embedded KRaft kafka server started at {}", bootstrapServers);
+
+        applicationProperties.setProperty(PROPERTY_PREFIX + ".bootstrap.servers", bootstrapServers);
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    }
+
+    private Properties buildBrokerConfig(File logDir, String configuredBootstrap) {
+        Properties brokerConfig = new Properties();
+
+        brokerConfig.setProperty("log.dir", logDir.getAbsolutePath());
+        brokerConfig.setProperty("delete.topic.enable", "true");
+        brokerConfig.setProperty("auto.create.topics.enable", "true");
+        brokerConfig.setProperty("group.initial.rebalance.delay.ms", "0");
+        brokerConfig.setProperty("offsets.topic.replication.factor", "1");
+        brokerConfig.setProperty("transaction.state.log.replication.factor", "1");
+        brokerConfig.setProperty("transaction.state.log.min.isr", "1");
+        brokerConfig.setProperty("num.partitions", "1");
+
+        String listener = toExternalListenerAddress(configuredBootstrap);
+
+        if (listener != null) {
+            brokerConfig.setProperty("listeners", listener + ",CONTROLLER://localhost:0");
+            brokerConfig.setProperty("advertised.listeners", listener);
+        }
+
+        String replicationFactor = properties.getProperty("offsets.topic.replication.factor");
+
+        if (replicationFactor != null) {
+            brokerConfig.setProperty("offsets.topic.replication.factor", replicationFactor);
+        }
+
+        return brokerConfig;
+    }
+
+    private void shutdownClusterQuietly() {
         if (cluster != null) {
             AtomicReference<Throwable> shutdownFailure = new AtomicReference<>();
 
@@ -96,59 +190,43 @@ public class EmbeddedKafkaServer implements Service {
 
             cluster = null;
         }
-
-        LOG.info("<== EmbeddedKafka.stop(isEmbedded={})", isEmbedded);
     }
 
-    private void startKraftBroker() throws Exception {
-        overrideExitMethods();
-
-        File logDir = constructDir("kafka");
-
-        LOG.info("Starting embedded KRaft kafka (log.dir={})", logDir.getAbsolutePath());
-
-        KafkaClusterTestKit.Builder clusterBuilder = new KafkaClusterTestKit.Builder(
-                new TestKitNodes.Builder()
-                        .setCombined(true)
-                        .setNumBrokerNodes(1)
-                        .setNumControllerNodes(1)
-                        .build());
-
-        Properties brokerConfig = buildBrokerConfig(logDir);
-
-        brokerConfig.forEach((key, value) -> clusterBuilder.setConfigProp(key.toString(), value));
-
-        cluster = clusterBuilder.build();
-        cluster.format();
-        cluster.startup();
-        cluster.waitForReadyBrokers();
-
-        String bootstrapServers = cluster.clientProperties().get(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG).toString();
-
-        LOG.info("Embedded KRaft kafka server started at {}", bootstrapServers);
-
-        applicationProperties.setProperty(PROPERTY_PREFIX + ".bootstrap.servers", bootstrapServers);
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-    }
-
-    private Properties buildBrokerConfig(File logDir) {
-        Properties brokerConfig = new Properties();
-
-        brokerConfig.setProperty("log.dir", logDir.getAbsolutePath());
-        brokerConfig.setProperty("delete.topic.enable", "true");
-        brokerConfig.setProperty("group.initial.rebalance.delay.ms", "0");
-        brokerConfig.setProperty("offsets.topic.replication.factor", "1");
-        brokerConfig.setProperty("transaction.state.log.replication.factor", "1");
-        brokerConfig.setProperty("transaction.state.log.min.isr", "1");
-        brokerConfig.setProperty("num.partitions", "1");
-
-        String replicationFactor = properties.getProperty("offsets.topic.replication.factor");
-
-        if (replicationFactor != null) {
-            brokerConfig.setProperty("offsets.topic.replication.factor", replicationFactor);
+    private static String toExternalListenerAddress(String bootstrapServers) {
+        if (StringUtils.isEmpty(bootstrapServers)) {
+            return null;
         }
 
-        return brokerConfig;
+        String hostPort = StringUtils.trim(bootstrapServers.split(",")[0]);
+
+        if (StringUtils.isEmpty(hostPort)) {
+            return null;
+        }
+
+        if (hostPort.contains("://")) {
+            return hostPort.replaceFirst("^PLAINTEXT://", "EXTERNAL://");
+        }
+
+        return "EXTERNAL://" + hostPort;
+    }
+
+    private static int parsePort(String bootstrapServers) {
+        if (StringUtils.isEmpty(bootstrapServers)) {
+            return -1;
+        }
+
+        String hostPort = StringUtils.trim(bootstrapServers.split(",")[0]);
+        int    colon    = hostPort.lastIndexOf(':');
+
+        if (colon < 0) {
+            return -1;
+        }
+
+        try {
+            return Integer.parseInt(hostPort.substring(colon + 1));
+        } catch (NumberFormatException e) {
+            return -1;
+        }
     }
 
     private void overrideExitMethods() {
