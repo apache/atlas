68d8d4aa490f1dc7c015f56dbada38f072ef1e46
aditya-gupta36 <aditya.gupta@freestoneinfotech.com>
2026-07-15 17:22:58 +0530

    ATLAS-4667: Cannot create subclassification using subTypes field

--- intg/src/main/java/org/apache/atlas/AtlasErrorCode.java
+++ intg/src/main/java/org/apache/atlas/AtlasErrorCode.java
@@ -184,6 +184,7 @@ public enum AtlasErrorCode {
     BLANK_VALUE_ATTRIBUTE(400, "ATLAS-400-00-105", "Value Attribute can't be empty!"),
     INVALID_RELATIONSHIP_LABEL(400, "ATLAS-400-00-106", "Invalid relationship label {0}. The referenced entity type {1} could not be resolved from the type registry."),
     NON_INDEXABLE_BM_DELETE_NOT_ALLOWED(400, "ATLAS-400-00-107", "Deletion not allowed for non-indexable Business Metadata ''{0}'' without force=true. Non-indexable attributes cannot be validated efficiently for references; use force=true to skip validation and delete (warning: orphaned references may remain)."),
+    CLASSIFICATIONDEF_SUBTYPES_NOT_ALLOWED(400, "ATLAS-400-00-107", "ClassificationDef {0}: subTypes is a derived field and cannot be specified during create/update. To establish a parent-child relationship, set superTypes on the child classification instead."),
 
     UNAUTHORIZED_ACCESS(403, "ATLAS-403-00-001", "{0} is not authorized to perform {1}"),
 
