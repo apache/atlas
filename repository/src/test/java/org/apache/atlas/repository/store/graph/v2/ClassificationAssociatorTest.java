a97e7ac9627ac31e2707e048ab3c9c4ae28d1212
Vinayak Marraiya <vinayak.marraiya@freestoneinfotech.com>
2026-08-19 11:46:05 +0530

    ATLAS-5380: Authorize bulk entity headers before returning classifications
    Apply per-entity ENTITY_READ checks in ClassificationAssociator.Retriever
    when building bulk headers response, matching getHeaderById behavior.

--- repository/src/test/java/org/apache/atlas/repository/store/graph/v2/ClassificationAssociatorTest.java
+++ repository/src/test/java/org/apache/atlas/repository/store/graph/v2/ClassificationAssociatorTest.java
@@ -44,6 +44,7 @@ import static org.apache.atlas.repository.store.graph.v2.ClassificationAssociato
 import static org.apache.atlas.repository.store.graph.v2.ClassificationAssociator.Updater.PROCESS_UPDATE;
 import static org.apache.atlas.repository.store.graph.v2.ClassificationAssociator.Updater.STATUS_DONE;
 import static org.apache.atlas.repository.store.graph.v2.ClassificationAssociator.Updater.STATUS_SKIPPED;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
@@ -174,6 +175,7 @@ public class ClassificationAssociatorTest {
             EntityGraphRetriever entityGraphRetriever = mock(EntityGraphRetriever.class);
 
             when(entityGraphRetriever.toAtlasEntityHeaderWithClassifications(TABLE_GUID)).thenReturn(entityHeaderWithClassification);
+            when(entityGraphRetriever.isEntityReadAllowed(any())).thenReturn(true);
 
             ClassificationAssociator.Retriever retriever = new ClassificationAssociator.Retriever(entityGraphRetriever, auditRepository);
 
